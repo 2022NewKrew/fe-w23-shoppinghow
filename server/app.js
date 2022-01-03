389606a3c951f86b905602694457d8a0b9f77c8c
fleek-moon <fleek.moon@kakaocorp.com>
2022-01-03 12:23:34 +0900

    feat : Set server

---
+++ server/app.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import path from 'path';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+app.use(cookieParser());
+
+app.use(express.static('client/build'));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve('../client', 'build', 'index.html'));
+});
+
+const port = 5000;
+
+app.listen(port, () => {
+  console.log(`Server Listening on ${port}`);
+});
+
+export default app;
