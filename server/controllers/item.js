9817fb2ff771ba5a8281c16eea0eed4d7a777c4b
fleek-moon <fleek.moon@kakaocorp.com>
2022-01-04 12:28:39 +0900

    feat : Add ItemController

---
+++ server/controllers/item.js
@@ -0,0 +1,40 @@
+import { ItemService } from '../services/item.js';
+import { error } from '../error/index.js';
+
+const getItems = async (req, res) => {
+  try {
+    const result = await ItemService.getItems();
+    return res.json({ success: true, result });
+  } catch (e) {
+    return res.json({ success: false, message: error.GET_ITEM_ERROR });
+  }
+};
+
+const getHotItems = async (req, res) => {
+  try {
+    const result = await ItemService.getHotItems();
+    return res.json({ success: true, result });
+  } catch (e) {
+    return res.json({ success: false, message: error.GET_HOT_ITEM_ERROR });
+  }
+};
+
+const getHotItemsName = async (req, res) => {
+  try {
+    const result = await ItemService.getHotItemsName();
+    return res.json({ success: true, result });
+  } catch (e) {
+    return res.json({ success: false, message: error.GET_HOT_ITEMS_NAME_ERROR });
+  }
+};
+
+const getHotDealItems = async (req, res) => {
+  try {
+    const result = await ItemService.getHotDealItems();
+    return res.json({ success: true, result });
+  } catch (e) {
+    return res.json({ success: false, message: error.GET_HOT_DEAL_ITEMS_ERROR });
+  }
+};
+
+export const ItemController = { getItems, getHotItems, getHotItemsName, getHotDealItems };
