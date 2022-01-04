18131ec11892687dc975c5fd01b516e1a9077ea4
김진옥 (Jade) <43373202+dragons6641@users.noreply.github.com>
2022-01-04 17:59:57 +0900

    [jade.kjo] 2주차 미션 PR - 1 (#16)
    * initial commit
    
    * move js code from index.html to app.js
    
    * remove package-lock.json from .gitignore
    
    Co-authored-by: jade.kjo <jade.kjo@kakaocorp.com>

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import "./styles/libs/reset.css";
+import "./sass/app.scss";
+
+function makeHotDealHTML() {
+  const target = document.querySelector(".hot-deal-list");
+  const hotdealItemTpl = `
+            <li class="hot-deal__item">
+                <a href="" class="hot-deal__link">
+                    <span class="hot-deal__thumb">
+                        <img src="//shop2.daumcdn.net/shophow/c/image/content/set/ad8255/20211221145844446_191895" class="hot-deal__img" alt="">
+                    </span>
+
+                    <strong class="hot-deal__title">구매1만↑우유앙빵10+10</strong>
+
+                    <span class="hot-deal__detail-price">
+                        <span class="txt-price">18,500 </span>
+                        <span class="txt-price-percent">핫딜가</span>
+                    </span>
+                </a>
+            </li>`;
+
+  target.innerHTML = Array(10)
+    .fill(0)
+    .reduce((html, item) => html + hotdealItemTpl, ``);
+}
+makeHotDealHTML();
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/app.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true,
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(ico|png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot|otf)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: [
+          { loader: "file-loader", options: { name: "[name].[ext]?[hash]" } },
+        ],
+      },
+    ],
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      minify: { collapseWhitespace: true },
+      hash: true,
+      template: "src/index.html",
+      title: "Development",
+    }),
+    new MiniCssExtractPlugin({ filename: "style.css" }),
+  ],
+};
