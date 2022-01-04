2669fb5711f72ed06f7101f860ab720936bb7c59
fleek-moon <fleek.moon@kakaocorp.com>
2022-01-04 15:33:49 +0900

    refactor : 함수 분리

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,22 @@
 import '@/static/styles/reset.scss';
-// import './index.scss';
+import '@/static/styles/index.scss';
 import { router, navigateTo } from '@/core/router';
 import { addMoneyUnitLogic } from '@/utils/helper';
 
 String.prototype.addMoneyUnit = addMoneyUnitLogic;
 
 window.addEventListener('popstate', router);
-document.body.addEventListener('click', (e) => {
+document.body.addEventListener('click', handleClickBody);
+
+const handleClickBody = (e) => {
+  changePage(e);
+};
+
+const changePage = (e) => {
   if (e.target.matches('[data-link]')) {
     e.preventDefault();
     navigateTo(e.target.href || e.target.dataset.link);
   }
-});
+};
+
 router();
