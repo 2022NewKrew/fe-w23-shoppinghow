73b93ffe71d54a68377351675278b56c7f95edd1
miro-ring <miro.ring@kakaocorp.com>
2022-01-04 14:45:30 +0900

    feat: 정적 파일 경로 사용응 위한 준비

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+    resolve: {
+        extensions: ['.js'],
+        alias: {
+            // '@components': path.resolve(__dirname, 'src/components'),
+            // '@scss': path.resolve(__dirname, 'src/scss'),
+            // '@api': path.resolve(__dirname, 'src/api'),
+        },
+    },
