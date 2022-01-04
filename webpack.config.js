1989c69847a3e74c5de94c52573578c934105507
h-yes-oo <hyesoo5115@naver.com>
2022-01-04 17:29:38 +0900

    Fix eslint, prettier, husky setting completed - change file structure

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+//여기도 추가
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/app.html',
+    }),
+  ],
+};
