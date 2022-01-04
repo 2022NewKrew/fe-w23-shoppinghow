7cec99df2e7b7b1b15102a40c79f1cc3187db8cc
rare.nick <rare.nick@kakaocorp.com>
2022-01-04 17:31:16 +0900

    하단 고정화면 생성 * dist 내용 gitignore에 추가 * 번들이름 고정(추후 수정예정)

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     output: {
         path: path.join(__dirname, "/backend/public/dist"),
         publicPath:"/dist",
-        filename: 'bundle.[contenthash].js',
+        filename: 'bundle..js',
     },
     module: {
         rules: [
