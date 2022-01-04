7f0861991a3fa7428c0d5ac013ba827bbb07506c
dlguswo333 <dlguswo002@gmail.com>
2022-01-04 23:55:53 +0900

    Implement complete Carousel

--- src/Carousel.js
+++ src/Carousel.js
@@ -10,10 +10,14 @@ export default class Carousel {
     this.container=container;
     this.length=this.container.children.length;
     /** @type {number} */
-    this.currentIndex=1;
+    this.currentIndex=0;
     /** @type {HTMLElement} */
     this.tabContainer=tabContainer;
     this.imageWidth=this.container.children[0].getBoundingClientRect().width;
+    /** @type {boolean} */
+    this.isBusy=false;
+    /** @type {number} */
+    this.transition=0.5;
 
     leftBtn.addEventListener("click", ()=>{
       this._goByOffset(-1);
@@ -21,12 +25,11 @@ export default class Carousel {
     rightBtn.addEventListener("click", ()=>{
       this._goByOffset(1);
     });
-
     
     tabContainer.innerHTML=Array(this.length).fill(0).map((_, index)=>(
       `<li data-tabIndex="${index}" class="planning__navigation-li"></li>`
     )).join("");
-    this._markCurrentPageActive();
+    this._markCurrentTabActive();
     tabContainer.addEventListener("click", (e)=>{
       const tabIndex=e.target.getAttribute("data-tabIndex");
       if(tabIndex===null){
@@ -41,36 +44,47 @@ export default class Carousel {
    * @param {number} targetIndex
    */
   goToIndexByNavgation(targetIndex){
-    this.currentIndex===targetIndex ?
-      undefined:
-      this._goByOffset(targetIndex, this.currentIndex<targetIndex);
+    this._goByOffset(targetIndex, this.currentIndex<targetIndex);
   }
+  
   /**
    * Do not call this outside.
    * @param {number} indexOffset
    */
   _goByOffset(indexOffset){
-    this.container.style=`transition: 0.5s ease-out;transform: translateX(${-indexOffset*(this.imageWidth)}px)`;
+    if(this.isBusy || indexOffset===0){
+      return;
+    }
+    this.isBusy=true;
+    this.currentIndex=(this.currentIndex+indexOffset+this.length)%this.length;
+    this._markCurrentTabActive();
+    let copyIndexOffset=indexOffset;
+    if(indexOffset<0){
+      while(copyIndexOffset!==0){
+        this.container.insertBefore(this.container.lastElementChild, this.container.firstElementChild);
+        copyIndexOffset+=1;
+      }
+      this.container.style=`transform: translateX(${indexOffset*(this.imageWidth)}px)`;
+      this.container.scrollWidth;
+      this.container.style=`transition: ${this.transition}s; ease-out;transform: 0`;
+    }
+    else {
+      this.container.style=`transition: ${this.transition}s ease-out; transform: translateX(${-indexOffset*(this.imageWidth)}px)`;
+    }
     this.container.ontransitionend=()=>{
       this.container.ontransitionend=null;
-      this.container.removeAttribute("style");
-      this.currentIndex=(this.currentIndex+indexOffset+this.length)%this.length;
-      this._markCurrentPageActive();
+      this.isBusy=false;
       if(indexOffset>0){
-        while(indexOffset!==0){
+        while(copyIndexOffset!==0){
           this.container.appendChild(this.container.firstElementChild);
-          indexOffset-=1;
-        }
+          copyIndexOffset-=1;
+        }  
       }
-      else{
-        while(indexOffset!==0){
-          this.container.insertBefore(this.container.lastElementChild, this.container.firstElementChild);
-          indexOffset+=1;
-        }
-      }
+      this.container.removeAttribute("style");
     };
   }
-  _markCurrentPageActive(){
+
+  _markCurrentTabActive(){
     Array.from(this.tabContainer.children).forEach((tab)=>{
       tab.classList.remove("planning__navigation-li-active");
     });
