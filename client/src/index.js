c839931240598cd6ba2d69c83c3941d22108771b
fleek-moon <fleek.moon@kakaocorp.com>
2022-01-04 14:37:33 +0900

    feat : 미션 - 프로토타입 객체 메서드 추가
    num인 돈을 string으로 바꿔주며 뒤에 '원'을 붙혀줌
    (만들게 없어서 만듬..)

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,11 @@
 import '@/static/styles/reset.scss';
 // import './index.scss';
 import { router, navigateTo } from '@/core/router';
+import { addMoneyUnitLogic } from '@/utils/helper';
+
+String.prototype.addMoneyUnit = addMoneyUnitLogic;
 
 window.addEventListener('popstate', router);
-
 document.body.addEventListener('click', (e) => {
   if (e.target.matches('[data-link]')) {
     e.preventDefault();
--- client/src/utils/helper.js
+++ client/src/utils/helper.js
@@ -2,3 +2,8 @@
 export const $ = (selector, parentNode = document) => {
   return parentNode.querySelector(selector);
 };
+
+export const addMoneyUnitLogic = (money) => {
+  const moneyStringType = money.toString().toLocaleString();
+  return moneyStringType + '원';
+};
