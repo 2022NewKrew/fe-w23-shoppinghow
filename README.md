3b9a85417214f722fca8f5136f8483a6d9ebe89c
jade.kjo <jade.kjo@kakaocorp.com>
2022-01-04 15:03:30 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# fe-w23-shoppinghow
-2,3주차 프로젝트 
+# 쇼핑하우 기본 레이아웃
+
+## 환경
+
+- BEM styles rules
+- SASS
+
+## 소스
+
+- /app/sass/\*.scss
+
+## 빌드
+
+```shell
+npm run sas
+```
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/app.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true,
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      title: "Development",
+    }),
+  ],
+};
