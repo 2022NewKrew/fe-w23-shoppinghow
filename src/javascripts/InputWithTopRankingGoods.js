39efd703489590143d28cfc181bca1120cebf9d5
brad.ley <brad.ley@kakaocorp.com>
2022-01-04 12:41:30 +0900

    add event to do not show roller text if input text was focused

--- src/javascripts/InputWithTopRankingGoods.js
+++ src/javascripts/InputWithTopRankingGoods.js
@@ -4,6 +4,8 @@ import RollingText from './RollingText'
 export default class InputWithTopRankingGoods {
     
     #ROLLING_PERIOD = 4000
+    #FOCUS_OUT_TIME_IF_MOUSE_OUT = 500
+    
     #TOP_10_GOODS_JSON_URL = 'http://localhost:3000/json/top-10-goods.json'
     
     #rootEl
@@ -40,8 +42,34 @@ export default class InputWithTopRankingGoods {
             })
         }
         
+        const setEventToDoNotShowRollerIfFocused = () => {
+            const inputEl = this.#rootEl.querySelector(`.${ styles.searchInput }`)
+            let timeId
+    
+            inputEl.addEventListener('focusin', () => {
+                this.#rollingText.rootEl.style.visibility = 'hidden'
+                this.#rollingText.stopAutoRolling()
+            })
+    
+            inputEl.addEventListener('focusout', () => {
+                this.#rollingText.rootEl.style.visibility = 'visible'
+                this.#rollingText.startAutoRolling()
+            })
+            
+            inputEl.addEventListener('mouseenter', () => {
+                clearTimeout(timeId)
+            })
+    
+            inputEl.addEventListener('mouseout', () => {
+                timeId = setTimeout(() => {
+                    inputEl.blur()
+                }, this.#FOCUS_OUT_TIME_IF_MOUSE_OUT)
+            })
+        }
+        
         createElement()
         createRollerText()
+        setEventToDoNotShowRollerIfFocused()
     }
     
     get rootEl() {
