8c2eeb2d6cccef66bd273f99d2abefe13080ccda
terajh <terajoohyun@ajou.ac.kr>
2022-01-04 13:56:13 +0900

    feat: 공통 컴포넌트 클래스파일 설정

---
+++ src/public/scripts/core/Component.js
@@ -0,0 +1,25 @@
+export class Component {
+    $target;
+    $state;
+    constructor($target) {
+        this.$target = $target;
+        this.setup();
+        this.setEvent();
+        this.render();
+    }
+
+    setUp() {}
+
+    getTemplate() {
+        return "";
+    }
+    render() {
+        this.$target.innerHTML = this.template();
+        this.setEvent();
+    }
+    setEvent() {}
+    setState(newState) {
+        this.$state = { ...this.$state, ...newState };
+        this.render();
+    }
+}
