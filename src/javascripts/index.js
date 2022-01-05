7ddf1adf35b29e1fd992a52f700b09ad23c6461f
brad.ley <brad.ley@kakaocorp.com>
2022-01-06 01:08:41 +0900

    add json file

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -4,21 +4,22 @@ import GoodsFloatingLayer from './Components/GoodsFloatingLayer'
 import GoodsCard from './Components/GoodsCard'
 import GoodsList from './Components/GoodsList'
 import SectionTitle from './Components/SectionTitle'
+import RecentGoodsButton from './Components/RecentGoodsButton'
 
 
 window.addEventListener('DOMContentLoaded', () => {
     const headerTopEl = document.querySelector('.header-top')
-    const headerMenuEl = document.querySelector('.header-menu')
+    const privateMenuEl = document.querySelector('.private-menu')
     const rollingText = new InputWithTopRankingGoods()
-    const recentGoodsFloatingLayer = new GoodsFloatingLayer()
+    const recentGoodsButton = new RecentGoodsButton()
     
     const target = document.querySelector('.container-wrapper')
     const hotDealTitle = new SectionTitle('품절주의, 역대급 핫딜')
     const hotDealGoodsList = new GoodsList('http://localhost:3000/json/hot-deal-goods.json')
     const keywordTitle = new SectionTitle('쇼핑 급상승 키워드')
-    const keywordGoodsList = new GoodsList('http://localhost:3000/json/hot-deal-goods.json')
+    const keywordGoodsList = new GoodsList('http://localhost:3000/json/keyword-goods.json')
     const howAboutTitle = new SectionTitle('이 상품 어때요?')
-    const howAboutGoodsList = new GoodsList('http://localhost:3000/json/hot-deal-goods.json')
+    const howAboutGoodsList = new GoodsList('http://localhost:3000/json/how-about-goods.json')
     
     target.appendChild(hotDealTitle.rootEl)
     target.appendChild(hotDealGoodsList.rootEl)
@@ -28,5 +29,5 @@ window.addEventListener('DOMContentLoaded', () => {
     target.appendChild(howAboutGoodsList.rootEl)
     
     headerTopEl.appendChild(rollingText.rootEl)
-    headerMenuEl.appendChild(recentGoodsFloatingLayer.rootEl)
+    privateMenuEl.appendChild(recentGoodsButton.rootEl)
 })
