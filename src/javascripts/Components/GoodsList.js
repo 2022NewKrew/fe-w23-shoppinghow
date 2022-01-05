74ede781e88da33671cc4c0f1c00059ba5ec0006
brad.ley <brad.ley@kakaocorp.com>
2022-01-05 23:00:26 +0900

    add Component 'GoodsList'

---
+++ src/javascripts/Components/GoodsList.js
@@ -0,0 +1,48 @@
+import Component from '../Component'
+import styles from '../../scss/goods-list.module.scss'
+import GoodsCard from './GoodsCard'
+
+export default class GoodsList extends Component {
+    
+    #goodsDataList
+    #goodsCardComponentList = []
+    
+    #goodListEl
+    
+    constructor(title, goodsJsonUrl) {
+        super(`
+            <div class="${ styles.component }">
+                <div class="${ styles.headerTitle }">${ title }</div>
+                <ul class="${ styles.goodsList }"></ul>
+            </div>
+        `)
+        
+        this.#goodListEl = this.rootEl.querySelector(`.${ styles.goodsList }`)
+        
+        this.#fetchGoodsData(goodsJsonUrl, (goodsDataList) => {
+            this.#goodsDataList = goodsDataList
+            this.update()
+        })
+    }
+    
+    update() {
+        this.#goodsCardComponentList.forEach((goodsCardComponent) => {
+            goodsCardComponent.remove()
+        })
+        
+        this.#goodsDataList.forEach((goodsData) => {
+            const goodsCardComponent = new GoodsCard(goodsData)
+            
+            this.addChild(goodsCardComponent, this.#goodListEl)
+            this.#goodsCardComponentList.push(goodsCardComponent)
+        })
+    }
+    
+    #fetchGoodsData(goodsJsonUrl, callbackAfterFetch) {
+        fetch(goodsJsonUrl)
+            .then((res) => {
+                res.json().then(callbackAfterFetch)
+            })
+    }
+    
+}
