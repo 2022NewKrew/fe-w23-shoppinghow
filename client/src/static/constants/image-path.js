f2ada59630bc4c1eaae1032d845678bb144a460c
fleek-moon <fleek.moon@kakaocorp.com>
2022-01-05 17:04:28 +0900

    chore : Add image path

---
+++ client/src/static/constants/image-path.js
@@ -0,0 +1,13 @@
+export const LOGO = 'https://search1.daumcdn.net/search/cdn/simage/shopping/v2/common/nav/logo_shw_2021.png';
+export const SEARCH_ICON =
+  'https://user-images.githubusercontent.com/50862052/148036293-c20028c4-5a82-401f-8c9f-c8fd83bb1a1c.png';
+export const CATEGORY_HOVER_ICON =
+  'https://user-images.githubusercontent.com/50862052/148036362-8247de79-537a-4420-a300-65232c9ad1e8.png';
+export const CATEGORY_ICON =
+  'https://user-images.githubusercontent.com/50862052/148036480-aa8a30f5-770d-4e7e-92ac-42a0f024352e.png';
+export const LOGIN_ICON =
+  'https://user-images.githubusercontent.com/50862052/148036551-c60f8420-28b1-4170-a49e-da964d395d97.png';
+export const RECENTLY_VIEWED_THINGS_ICON =
+  'https://user-images.githubusercontent.com/50862052/148036792-537918e6-9e3d-4b3c-97dd-e9550d4d791d.png';
+export const USER_ICON =
+  'https://user-images.githubusercontent.com/50862052/148036850-f00a4b23-f9c8-447e-a433-cdf94263376f.png';
