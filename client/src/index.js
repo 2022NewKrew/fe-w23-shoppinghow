498140299e6038827ce49cd1b0d59b642720fa6d
fleek-moon <fleek.moon@kakaocorp.com>
2022-01-05 19:17:11 +0900

    feat : import header in root

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,11 @@ import '@/static/styles/reset.scss';
 import '@/static/styles/index.scss';
 import { router, navigateTo } from '@/core/router';
 import { addMoneyUnitLogic } from '@/utils/helper';
+import Header from './components/header/header-whole';
 
 String.prototype.addMoneyUnit = addMoneyUnitLogic;
+
+new Header({ $parent: document.body });
 
 window.addEventListener('popstate', router);
 document.body.addEventListener('click', handleClickBody);
