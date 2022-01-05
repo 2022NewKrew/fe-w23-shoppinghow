05ab885ba2e6dac688b033b647c53438565aabe5
vvvpiano <vvvpiano60@gmail.com>
2022-01-06 02:20:08 +0900

    feat: 캐로셀 컴포넌트 구현

---
+++ src/components/carousel/CarouselButtons.js
@@ -0,0 +1,6 @@
+export class CarouselButtons {
+    constructor(onClickHandler, transitionTime) {
+        this.onClickHandler = onClickHandler;
+        this.transitionTime = transitionTime;
+    }
+}
