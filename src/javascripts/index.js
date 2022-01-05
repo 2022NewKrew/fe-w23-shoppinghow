2d90258756877a069442afc5292c769150156aa5
brad.ley <brad.ley@kakaocorp.com>
2022-01-06 00:49:04 +0900

    add test code

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -2,6 +2,8 @@ import css from '../scss/main.scss'
 import InputWithTopRankingGoods from './Components/InputWithTopRankingGoods'
 import GoodsFloatingLayer from './Components/GoodsFloatingLayer'
 import GoodsCard from './Components/GoodsCard'
+import GoodsList from './Components/GoodsList'
+import SectionTitle from './Components/SectionTitle'
 
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -10,14 +12,20 @@ window.addEventListener('DOMContentLoaded', () => {
     const rollingText = new InputWithTopRankingGoods()
     const recentGoodsFloatingLayer = new GoodsFloatingLayer()
     
-    const target = document.querySelector('.hot-deal-list')
-    const goodsCard = new GoodsCard({
-        title: '육즙가득 안심스테이크1+1',
-        imgSrc: 'http://shop2.daumcdn.net/shophow/c/image/content/set/ad8255/20211221145844446_191895',
-        price: '27,700'
-    })
+    const target = document.querySelector('.container-wrapper')
+    const hotDealTitle = new SectionTitle('품절주의, 역대급 핫딜')
+    const hotDealGoodsList = new GoodsList('http://localhost:3000/json/hot-deal-goods.json')
+    const keywordTitle = new SectionTitle('쇼핑 급상승 키워드')
+    const keywordGoodsList = new GoodsList('http://localhost:3000/json/hot-deal-goods.json')
+    const howAboutTitle = new SectionTitle('이 상품 어때요?')
+    const howAboutGoodsList = new GoodsList('http://localhost:3000/json/hot-deal-goods.json')
     
-    target.appendChild(goodsCard.rootEl)
+    target.appendChild(hotDealTitle.rootEl)
+    target.appendChild(hotDealGoodsList.rootEl)
+    target.appendChild(keywordTitle.rootEl)
+    target.appendChild(keywordGoodsList.rootEl)
+    target.appendChild(howAboutTitle.rootEl)
+    target.appendChild(howAboutGoodsList.rootEl)
     
     headerTopEl.appendChild(rollingText.rootEl)
     headerMenuEl.appendChild(recentGoodsFloatingLayer.rootEl)
