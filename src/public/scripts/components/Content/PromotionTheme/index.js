ca3a9a5038384520f86d08affaef6fe64a0ad9bf
terajh <terajoohyun@ajou.ac.kr>
2022-01-06 14:03:59 +0900

    feat: header Menu CSS 수정 및 프로모션 테마 컴포넌트 코드 cleaning

--- src/public/scripts/components/Content/PromotionTheme/index.js
+++ src/public/scripts/components/Content/PromotionTheme/index.js
@@ -11,36 +11,36 @@ export default class PromotionTheme extends Component {
   template() {
     const { items } = this.$state;
     return `
-    <ul class="theme-container">
+      <ul class="theme-container">
         ${
           items.length > 0 &&
-          items.map(
-            item => `
-        <li class="theme-item">
-            <a href="${item.href}" class="theme__link">
-                <span class="theme-item__info">
-                <img
-                    src="${item.src}"
-                    width="${item.width}px"
-                    height="${item.height}px"
-                    class="img_top"
-                    alt="${item.title}" /></span
-                >
-                <strong class="theme-item__title">${item.title}</strong>
-                <span class="theme-item__desc">${item.info}</span
-                ><span class="theme-item__icon">테마</span></a
-            >
-        </li>
-        `,
-          )
+          items
+            .map(
+              item => `
+              <li class="theme-item">
+                  <a href="${item.href}" class="theme__link">
+                      <span class="theme-item__info">
+                      <img
+                          src="${item.src}"
+                          width="${item.width}px"
+                          height="${item.height}px"
+                          class="img_top"
+                          alt="${item.title}" /></span
+                      >
+                      <strong class="theme-item__title">${item.title}</strong>
+                      <span class="theme-item__desc">${item.info}</span
+                      ><span class="theme-item__icon">테마</span></a
+                  >
+              </li>
+              `,
+            )
+            .join("")
         }
-    
     </ul>
     `;
   }
   async mounted() {
     const { result } = await api.get("event/items");
-    console.log(result);
     if (JSON.stringify(this.$state.items) !== JSON.stringify(result)) {
       this.setState({ items: result });
     }
