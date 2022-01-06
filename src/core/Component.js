073ef6e9c3b5f1b67d58d58886a4a79fd8174494
lin.s <lin.s@kakaocorp.com>
2022-01-07 01:30:26 +0900

    Refactor header and navbar layout

--- src/core/Component.js
+++ src/core/Component.js
@@ -1,35 +1,39 @@
 export default class Component {
   $target;
-  $props;
-  $state;
+  props;
+  state;
+
   constructor($target, props = {}) {
     this.$target = $target;
-    this.$props = props;
+    this.props = props; // props 할당
     this.setup();
-    this.setEvent();
     this.render();
   }
+
   setup() {}
 
+  // 자식 컴포넌트를 마운트
   mounted() {}
 
   template() {
     return "";
   }
+
   render() {
     this.$target.innerHTML += this.template();
     this.setEvent();
-    this.mounted();
+    this.mounted(); // render 후에 mounted가 실행 된다.
   }
 
+  // 이벤트 적용
   setEvent() {}
 
   setState(newState) {
-    this.$state = { ...this.$state, ...newState };
+    this.state = { ...this.state, ...newState };
     this.render();
   }
 
-  addEvent(eventType, selector, callback, options) {
+  addEvent(eventType, selector, callback, options = {}) {
     const children = [...this.$target.querySelectorAll(selector)];
     // selector에 명시한 것 보다 더 하위 요소가 선택되는 경우가 있을 땐
     // closest를 이용하여 처리한다.
