bd968c42888ec55fc7c4e804e569cf5e398a8885
bepyan <bepyan@naver.com>
2022-01-06 18:48:58 +0900

    fix. outerHTML 교체시에도 클래스이름 유지

--- src/core/Component.js
+++ src/core/Component.js
@@ -1,3 +1,5 @@
+import { htmlToElement } from '@utils';
+
 export default class Component {
   /**
    * @param {HTMLElement} $target
@@ -7,24 +9,23 @@ export default class Component {
 
   /**
    * @param {HTMLElement} $target 타겟 기준으로 컴포넌트를 삽입합니다.
-   * @param props
-   * `renderType: "innerHTML" | "outerHTML" | beforebegin | afterbegin | beforeend | afterend`
-   * insertAdjacentHTML의 position https://developer.mozilla.org/ko/docs/Web/API/Element/insertAdjacentHTML
+   * @param { { renderType: "innerHTML" | "outerHTML" | "beforebegin" | "afterbegin" | "beforeend" | "afterend" } } props
+   *
+   * outerHTML은 내부적으로 `replaceWith`로 구현되어 있습니다.
+   * insertAdjacentHTML의 position 참고하세요. https://developer.mozilla.org/ko/docs/Web/API/Element/insertAdjacentHTML
    */
   constructor($target, props = {}) {
     this.$target = $target;
     this.props = { renderType: 'innerHTML', ...props };
 
     this.setup();
-    this.setEvent();
     this.render();
+    this.setEvent();
   }
 
   setup() {}
 
-  setEvent() {
-    this.addEvent('click', 'div', (e) => {});
-  }
+  setEvent() {}
   addEvent(eventType, selector, callback) {}
 
   template() {
@@ -37,10 +38,10 @@ export default class Component {
     if (renderType === 'innerHTML') {
       this.$target.innerHTML = this.template();
     } else if (renderType === 'outerHTML') {
-      const $parent = this.$target.parentElement;
-      this.$target.outerHTML = this.template();
-      $parent.querySelector();
-      console.log(this.$target);
+      const element = htmlToElement(this.template());
+      // this.$target.replaceWith(element);
+      this.$target.className = element.className;
+      this.$target.innerHTML = element.innerHTML;
     } else {
       this.$target.insertAdjacentHTML(renderType, this.template());
     }
