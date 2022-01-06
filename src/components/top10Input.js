a32f819f3de0abca5b9d8dbb15519aea7537ea88
lin.s <lin.s@kakaocorp.com>
2022-01-07 04:48:03 +0900

    Fix top10 carousel - async await

--- src/components/top10Input.js
+++ src/components/top10Input.js
@@ -1,6 +1,6 @@
 import Component from "../core/Component";
 
-const ROLLING_TIME = 3000;
+const ROLLING_TIME = 2000;
 const HEIGHT = 60;
 // const TRANSITION_DURATION = "500ms";
 const FOCUS_OUT_TIME = 500;
@@ -46,7 +46,7 @@ export default class Top10Input extends Component {
   setEvent() {
     const searchInput = ".search__input";
     const top10Container = this.$target.querySelector("#top10Container");
-    // const input = document.querySelector(".search__input");
+
     let timeId;
 
     this.addEvent("mouseover", searchInput, this.pauseSlide.bind(this));
@@ -68,7 +68,7 @@ export default class Top10Input extends Component {
       searchInput,
       (e) => {
         e.target.style.backgroundColor = "transparent";
-        this.runSlide(top10Container);
+        this.runSlide();
       },
       true
     );
@@ -88,13 +88,15 @@ export default class Top10Input extends Component {
     const $prevItem = $top10Container.querySelector(".previous-top-item");
     const $curItem = $top10Container.querySelector(".current-top-item");
     const $nextItem = $top10Container.querySelector(".next-top-item");
-    // console.log($prevItem, $curItem, $nextItem);
+
+    $curItem.classList.add("current-top-item");
+    $nextItem.classList.add("next-top-item");
 
     this.slidePlaying = setTimeout(() => {
       $prevItem.classList.remove("previous-top-item");
 
-      $curItem.classList.remove("current-top-item");
       $curItem.classList.add("previous-top-item");
+      $curItem.classList.remove("current-top-item");
 
       $nextItem.classList.remove("next-top-item");
       $nextItem.classList.add("current-top-item");
@@ -120,146 +122,3 @@ export default class Top10Input extends Component {
     }
   }
 }
-
-// const ROLLING_TIME = 3000;
-// const HEIGHT = 60;
-// const TRANSITION_DURATION = "500ms";
-// const FOCUS_OUT_TIME = 500;
-
-// class Top10Input2 {
-//   #top10Idx;
-//   #top10List;
-//   #slidePlaying;
-
-//   constructor() {
-//     this.#top10Idx = 0;
-//     this.#top10List = this.#fetchTop10List();
-//   }
-
-//   #fetchTop10List() {
-//     const top10 = require("../data/searchTop10.json").top10;
-//     return top10;
-//   }
-
-//   findIndex(top10List) {
-//     top10List.forEach((e, idx) => {
-//       if (e.classList.contains("current-top-item")) {
-//         this.#top10Idx = idx;
-//       }
-//     });
-//   }
-
-//   findActiveSlide(top10Container) {
-//     // current active-slide 찾기
-//     const top10List = Array.from(top10Container.children);
-//     const top10ListLength = top10List.length;
-//     this.findIndex(top10List);
-
-//     const prevItem =
-//       top10Container.children[
-//         this.#top10Idx - 1 < 0 ? top10ListLength - 1 : this.#top10Idx - 1
-//       ];
-//     const curItem = top10Container.children[this.#top10Idx];
-//     const nextItem =
-//       top10Container.children[
-//         this.#top10Idx + 1 === top10ListLength ? 0 : this.#top10Idx + 1
-//       ];
-//     const nextNextItem =
-//       top10Container.children[
-//         this.#top10Idx + 2 >= top10ListLength
-//           ? this.#top10Idx - top10ListLength + 2
-//           : this.#top10Idx + 2
-//       ];
-
-//     return [prevItem, curItem, nextItem, nextNextItem];
-//   }
-
-//   initSlide() {
-//     top10Container.style.transitionDuration = TRANSITION_DURATION;
-
-//     const [prevItem, curItem, nextItem] = this.findActiveSlide(top10Container);
-
-//     curItem.classList.add("current-top-item");
-//     nextItem.classList.add("next-top-item");
-//   }
-
-//   runSlide(top10Container) {
-//     const run = () => {
-//       top10Container.style.transitionDuration = TRANSITION_DURATION;
-
-//       // previous, current, next active-slide 찾기
-//       const [prevItem, curItem, nextItem, nextNextItem] =
-//         this.findActiveSlide(top10Container);
-
-//       prevItem.classList.remove("previous-top-item");
-
-//       curItem.classList.remove("current-top-item");
-//       curItem.classList.add("previous-top-item");
-
-//       nextItem.classList.remove("next-top-item");
-//       nextItem.classList.add("current-top-item");
-
-//       nextNextItem.classList.add("next-top-item");
-//     };
-
-//     this.#slidePlaying = setInterval(run, ROLLING_TIME);
-//   }
-
-//   pauseSlide() {
-//     clearInterval(this.#slidePlaying);
-//   }
-
-//   addSlideEventListener(input) {
-//     let timeId;
-
-//     input.addEventListener("mouseover", (e) => {
-//       clearTimeout(timeId);
-//     });
-
-//     input.addEventListener("mouseout", (e) => {
-//       timeId = setTimeout(() => {
-//         input.blur();
-//       }, FOCUS_OUT_TIME);
-//     });
-
-//     input.addEventListener("focus", (e) => {
-//       input.style.backgroundColor = "white";
-//       this.pauseSlide();
-//     });
-
-//     input.addEventListener("blur", (e) => {
-//       input.style.backgroundColor = "transparent";
-//       this.runSlide(top10Container);
-//     });
-//   }
-
-//   render() {
-//     window.addEventListener("DOMContentLoaded", () => {
-//       const top10Container = document.querySelector("#top10Container");
-//       const input = document.querySelector(".search__input");
-//       this.initSlide(top10Container);
-//       this.runSlide(top10Container);
-//       this.addSlideEventListener(input);
-//     });
-
-//     return `
-//         <div class="search">
-//             <form>
-//               <input type="text" class="search__input" />
-//               <button class="search__icon">🔍</button>
-//             </form>
-//             <div class="search__box">
-//                 <ul class="search-top10" id="top10Container">
-//                 ${this.#top10List
-//                   .map(
-//                     ({ rank, title }) => `
-//                     <li class="search-top10__item">${rank}. ${title}</li>
-//                     `
-//                   )
-//                   .join("")}
-//                 </ul>
-//             </div>
-//         </div>
-//       `;
-//   }
-// }
--- src/core/Component.js
+++ src/core/Component.js
@@ -1,7 +1,6 @@
 export default class Component {
   $target;
   props;
-  state;
 
   constructor($target, props = {}) {
     this.$target = $target;
@@ -21,24 +20,20 @@ export default class Component {
 
   init() {}
 
-  render() {
+  async render() {
     this.$target.innerHTML += this.template();
-    this.setEvent();
-    this.mounted(); // render 후에 mounted가 실행 된다.
+    await this.setEvent();
+    await this.mounted(); // render 후에 mounted가 실행 된다.
   }
 
   // 이벤트 적용
   setEvent() {}
 
-  setState(newState) {
-    this.state = { ...this.state, ...newState };
-    this.render();
-  }
-
   addEvent(eventType, selector, callback, options = {}) {
     const children = [...this.$target.querySelectorAll(selector)];
     // selector에 명시한 것 보다 더 하위 요소가 선택되는 경우가 있을 땐
     // closest를 이용하여 처리한다.
+
     const isTarget = (target) =>
       children.includes(target) || target.closest(selector);
     this.$target.addEventListener(
