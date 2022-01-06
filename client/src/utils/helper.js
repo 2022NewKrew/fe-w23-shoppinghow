d1fa7ac516202306b6e55e9d781f5d253baad190
fleek-moon <fleek.moon@kakaocorp.com>
2022-01-06 15:30:13 +0900

    feat : 할인 적용 해주는 유틸 추가

--- client/src/utils/helper.js
+++ client/src/utils/helper.js
@@ -7,3 +7,10 @@ export const addMoneyUnitLogic = (money) => {
   const moneyStringType = money.toString().toLocaleString();
   return moneyStringType + '원';
 };
+
+export const adjustDiscount = (price, discountRate) => {
+  const discountRateWithoutPerCent = discountRate.replace(/[^0-9]/g, '');
+  const adjustDiscountPrice = Number(discountRateWithoutPerCent) * 0.01 * price;
+  const stringPrice = adjustDiscountPrice.toLocaleString();
+  return stringPrice + '원';
+};
