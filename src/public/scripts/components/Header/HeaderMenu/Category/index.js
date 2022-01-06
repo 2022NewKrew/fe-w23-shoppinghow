aefd3f7daa4428d8f8751ba293edfdb5d6fc54a4
terajh <terajoohyun@ajou.ac.kr>
2022-01-06 11:20:11 +0900

    feat: 카테고리 메뉴 -> 마우스 hover 이벤트 작업 + css 추가

--- src/public/scripts/components/Header/HeaderMenu/Category/index.js
+++ src/public/scripts/components/Header/HeaderMenu/Category/index.js
@@ -1,30 +1,40 @@
 import { $ } from "@utils/query.js";
 import { Component } from "@core/Component";
+import CategoryContent from "./CategoryContent";
 
 export default class Category extends Component {
-  setUp() {}
+  setUp() {
+    this.$state = {
+      categoryDisplay: false,
+    };
+  }
   template() {
+    const { categoryDisplay } = this.$state.categoryDisplay;
     return `
         <button class="category__title"><i class="fas fa-bars"></i>카테고리</button>
-        <ul class="category-first">
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        </ul>
-        <ul class="category-second">
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        </ul>
-        <ul class="category-third">
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        </ul>
+        <div component="category-content" class="category__content"></div>
     `;
   }
-  setEvent() {}
+  mounted() {
+    const $categoryContent = $('[component="category-content"]', this.$target);
+
+    new CategoryContent($categoryContent, { categoryDisplay: this.$state.categoryDisplay });
+  }
+  setEvent() {
+    $(".category__title", this.$target).addEventListener("mouseover", event => {
+      const { target } = event;
+      if (this.checkButtonTag(target)) {
+        target.nextElementSibling.style.display = "block";
+      }
+    });
+    $(".category__title", this.$target).addEventListener("mouseout", event => {
+      const { target } = event;
+      if (this.checkButtonTag(target)) {
+        target.nextElementSibling.style.display = "none";
+      }
+    });
+  }
+  checkButtonTag(target) {
+    return target.tagName === "BUTTON";
+  }
 }
