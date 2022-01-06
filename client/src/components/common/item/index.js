02795a471ceeb40e22cfc5974e17a7f0d28bd53f
fleek-moon <fleek.moon@kakaocorp.com>
2022-01-06 15:33:50 +0900

    feat : Add Item Component

---
+++ client/src/components/common/item/index.js
@@ -0,0 +1,44 @@
+import './index.scss';
+import { adjustDiscount } from '@/utils/helper';
+
+export default class Item {
+  constructor({ $parent, info, index }) {
+    const item = document.createElement('li');
+    item.className = 'item';
+    item.innerHTML = `
+        <a href="" class="item-link">
+           ${this.createHotItemName(info, index)}
+            <img src=${info.imageSrc} class="item-img" alt="상품 이미지">
+            <strong class="item-title">${this.createItemTitle(info)}</strong>
+            <span class="item-detail-price">
+                ${this.createPrice(info)}
+            </span>
+        </a>`;
+    $parent.appendChild(item);
+  }
+
+  createHotItemName(info, index) {
+    return !info.discount ? `<span class='hot-item-title'>${index + 1}. ${info.title}</span>` : '';
+  }
+
+  createItemTitle(info) {
+    return !info.discount ? info.content : info.title;
+  }
+
+  createPrice(info) {
+    return info.discount ? this.createHotDealPrice(info.discount, info.price) : this.createHotItemPrice(info.price);
+  }
+
+  createHotDealPrice(discount, price) {
+    return discount === '핫딜가'
+      ? `<span class="txt-price">${price.toLocaleString()}원</span>
+         <span class="txt-price-percent">핫딜가</span>`
+      : `<span class="txt-price">${adjustDiscount(price, discount)}</span>
+         <span class="txt-price-before-discount">${price.toLocaleString()}원</span>
+         <span class="txt-price-percent">${discount}</span>`;
+  }
+
+  createHotItemPrice(price) {
+    return `<span class="txt-price">${price.toLocaleString()}원</span>`;
+  }
+}
