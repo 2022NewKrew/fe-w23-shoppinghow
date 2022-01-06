517334d038155a0409ceffeba0d2bafd9720b554
lin.s <lin.s@kakaocorp.com>
2022-01-07 01:52:33 +0900

    Refactor main layout to class component

--- src/components/banners.js
+++ src/components/banners.js
@@ -1,26 +1,46 @@
+import Component from "../core/Component";
 import Carousel from "../components/Carousel";
 
-export default function createBanners() {
+export default class Banners extends Component {
+  template() {
+    return `
+    <div class="banner">
+      <div class="best">
+        <a href="#" class="best__link">
+          <img
+            src="//shop1.daumcdn.net/shophow/sib/0_211210142533_BedHMJMFxJiJcYPqWFiZwzldCrXJHrcC"
+            width="485"
+            height="340"
+            class="img_g"
+            alt="기획전 이벤트"
+          />
+        </a>
+      </div>
+    `;
+  }
+}
+
+function createBanners() {
   const banner = document.createElement("div");
   const carousel = new Carousel();
 
   banner.insertAdjacentHTML(
     "afterbegin",
     `<div class="banner">
-    <div class="best">
-      <a href="#" class="best__link">
-        <img
-          src="//shop1.daumcdn.net/shophow/sib/0_211210142533_BedHMJMFxJiJcYPqWFiZwzldCrXJHrcC"
-          width="485"
-          height="340"
-          class="img_g"
-          alt="기획전 이벤트"
-        />
-      </a>
+      <div class="best">
+        <a href="#" class="best__link">
+          <img
+            src="//shop1.daumcdn.net/shophow/sib/0_211210142533_BedHMJMFxJiJcYPqWFiZwzldCrXJHrcC"
+            width="485"
+            height="340"
+            class="img_g"
+            alt="기획전 이벤트"
+          />
+        </a>
+      </div>
+      ${carousel.render()}
+      
     </div>
-    ${carousel.render()}
-    
-  </div>
       
   `
   );
