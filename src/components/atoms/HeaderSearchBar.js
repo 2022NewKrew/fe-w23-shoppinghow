7ac407a312a8ef1af1d9c4a82cce7b51f7f80ec9
bepyan <bepyan@naver.com>
2022-01-07 12:23:18 +0900

    feat. 검색바 포커스 에니메이션 & 부드럽게 unmount

--- src/components/atoms/HeaderSearchBar.js
+++ src/components/atoms/HeaderSearchBar.js
@@ -8,6 +8,8 @@ export class HeaderSearchBar extends Component {
     this.maxIndex = top10List.length - 1;
     this.AUTO_SLIDE_TIME = 2000;
     this.SLIDE_FRAME_HEIGHT = 30;
+    this.FOCUSE_SEARCH_CLASSNAME = 'search--focus';
+    this.HIDE_TOP10MODAL_CLASSNAME = 'top10modal--hide';
   }
 
   template() {
@@ -40,7 +42,7 @@ export class HeaderSearchBar extends Component {
                 ${top10TrackList.map(top10Template).join('')}
             </ul>
         </div>
-        <div class="top10modal">
+        <div class="top10modal ${this.HIDE_TOP10MODAL_CLASSNAME}" tabindex="-1">
             <span class="top10modal__title">인기 쇼핑 키워드</span>
             <ol class="top10modal__keywrodList">
                 ${top10List.slice(0, 5).map(to10ModalItemTemplate).join('')}
@@ -54,34 +56,63 @@ export class HeaderSearchBar extends Component {
   }
 
   mounted() {
-    this.$top10Modal = $('.top10modal', this.$target);
-
     this.$sliderTrack = $('.top10__track', this.$target);
     this.$sliderTrack.addEventListener('transitionend', () => {
       this.onSlideEnd();
     });
 
-    this.$input = $('.search__input', this.$target);
-    this.$input.addEventListener('focus', () => {
-      this.clearTimerAction();
-      this.$sliderTrack.style.display = 'none';
+    this.$search = this.$target.closest('.search');
 
-      this.$input.closest('.search').classList.add('search--focus');
+    this.$search.addEventListener('focusin', () => this.focusinSearch());
+    this.$search.onmouseleave = () => {
+      if (this.isSearchFocused()) this.focusoutSearch();
+    };
 
-      this.$top10Modal.classList.add('top10modal--show');
-    });
-    this.$input.addEventListener('blur', () => {
-      this.$sliderTrack.style.display = 'block';
-      this.$input.closest('.search').classList.remove('search--focus');
-      this.$top10Modal.classList.remove('top10modal--show');
+    this.$top10Modal = $('.top10modal', this.$target);
+    this.$top10Modal.ontransitionend = () => {
+      if (!this.isSearchFocused()) this.disactiveTop10Modal();
+    };
 
-      if (!this.timerId) this.startTimerAction();
-    });
-
-    this.startTimerAction();
+    this.startSlideTimer();
   }
 
-  // util
+  /* util */
+
+  // search focus animation
+
+  isSearchFocused() {
+    return this.$search.classList.contains(this.FOCUSE_SEARCH_CLASSNAME);
+  }
+
+  focusinSearch() {
+    this.$search.classList.add(this.FOCUSE_SEARCH_CLASSNAME);
+    this.showTop10Modal();
+  }
+
+  focusoutSearch() {
+    this.$search.classList.remove(this.FOCUSE_SEARCH_CLASSNAME);
+    $('.search__input', this.$search).blur();
+    this.hideTop10Modal();
+  }
+
+  showTop10Modal() {
+    this.$top10Modal.classList.remove(this.HIDE_TOP10MODAL_CLASSNAME);
+    this.$top10Modal.style.opacity = 1;
+    this.$sliderTrack.style.display = 'none';
+    this.clearSlideTimer();
+  }
+
+  hideTop10Modal() {
+    this.$top10Modal.style.opacity = 0;
+    this.$sliderTrack.style.display = 'block';
+    if (!this.timerId) this.startSlideTimer();
+  }
+
+  disactiveTop10Modal() {
+    this.$top10Modal.classList.add(this.HIDE_TOP10MODAL_CLASSNAME);
+  }
+
+  // slide animation
 
   onSlide(index) {
     this.$sliderTrack.style.transition = 'transform 400ms ease-in-out';
@@ -101,13 +132,14 @@ export class HeaderSearchBar extends Component {
       this.$sliderTrack.style.transition = 'none';
       this.moveTrack();
     }
-    if (!this.timerId) this.startTimerAction();
+    if (!this.timerId) this.startSlideTimer();
   }
 
-  clearTimerAction() {
+  clearSlideTimer() {
     this.timerId = clearInterval(this.timerId);
   }
-  startTimerAction() {
+
+  startSlideTimer() {
     this.timerId = setInterval(() => this.onSlide(this.currentIndex + 1), this.AUTO_SLIDE_TIME);
   }
 }
