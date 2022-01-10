0843782bc2af9d7b52a5244ddbef5977edb88c81
vvvpiano <vvvpiano60@gmail.com>
2022-01-10 17:46:22 +0900

    refactor: searchTop10 내부를 verticalRolling 컴포넌트로 이전

--- src/components/header/searchTop10.js
+++ src/components/header/searchTop10.js
@@ -12,54 +12,11 @@ const dummyData = [
     '콩나물',
 ];
 
-const addRollingAnimation = (top10Container) => {
-    const rollingIntervalTime = 3000;
-    let count = 0;
-    const rollItem = (count, top10Container) => () => {
-        const rollingTransitionTime = 300;
-        const itemHeight = 30;
-        count++;
-        top10Container.style.transitionDuration = rollingTransitionTime + 'ms';
-        top10Container.style.transform = `translateY(-${count * itemHeight}px)`;
-        top10Container.ontransitionend = () => {
-            if (count == top10Container.children.length - 1) {
-                console.log('end');
-                top10Container.style.transitionDuration = '0ms';
-                top10Container.style.transform = 'translateY(0px)';
-                count = 0;
-            }
-            setTimeout(rollItem(count, top10Container), rollingIntervalTime);
-        };
-    };
-
-    setTimeout(rollItem(count, top10Container), rollingIntervalTime);
-};
-
-const copySpareFirst = (itemArray) => {
-    const firstNode = itemArray[0].cloneNode(true);
-    return [...itemArray, firstNode];
-};
-
-const renderTop10List = (dummyData) => {
-    const top10Items = dummyData.map((title, i) => {
-        const item = document.createElement('div');
-        item.classList.add('search-top10__item');
-        item.innerHTML = `<span class="search-top10__item--numbering">${i + 1}</span><div>${title}</div>`;
-        return item;
-    });
-    const top10 = document.createElement('ol');
-    top10.classList.add('search-top10__container');
-    top10.append(...copySpareFirst(top10Items));
-    addRollingAnimation(top10);
-    return top10;
-};
-
 // TODO: parent 없도록 리팩토링하기
 export const searchTop10 = (parent) => {
     const target = parent.querySelector('.search-top10__window');
 
     const render = () => {
-        target.appendChild(renderTop10List(dummyData));
         // return target;
     };
 
---
+++ src/components/verticalRolling/verticalRolling.js
@@ -0,0 +1,59 @@
+const appendFirstClone = (itemArray) => {
+    const firstNode = itemArray[0].cloneNode(true);
+    return [...itemArray, firstNode];
+};
+
+const addRollingAnimation = (container) => {
+    const rollingIntervalTime = 3000;
+    let count = 0;
+    const rollItem = (count, container) => () => {
+        const rollingTransitionTime = 300;
+        const itemHeight = 30;
+        count++;
+        container.style.transitionDuration = rollingTransitionTime + 'ms';
+        container.style.transform = `translateY(-${count * itemHeight}px)`;
+        container.ontransitionend = () => {
+            if (count == top10Container.children.length - 1) {
+                console.log('end');
+                container.style.transitionDuration = '0ms';
+                container.style.transform = 'translateY(0px)';
+                count = 0;
+            }
+            setTimeout(rollItem(count, container), rollingIntervalTime);
+        };
+    };
+
+    setTimeout(rollItem(count, container), rollingIntervalTime);
+};
+
+const renderItem = (item, index) => {
+    const item = document.createElement('div');
+    item.classList.add('vertical-rolling__item');
+    item.innerHTML = `
+        <span class="vertical-rolling__item--numbering">${index + 1}</span>
+        <div>${title}</div>
+    `;
+    return item;
+};
+
+const renderVerticalList = (dummyData) => {
+    const itemDOMArray = dummyData.map((title, i) => renderItem(title, i));
+    const listContainer = document.createElement('ol');
+    listContainer.className = 'vertical-rolling__container';
+    listContainer.append(...appendFirstClone(itemDOMArray));
+    addRollingAnimation(listContainer);
+    return listContainer;
+};
+
+export const verticalRolling = (contentsData) => {
+    const verticalRollingWindow = document.createElement('div');
+    verticalRollingWindow.classList.add('vertical-rolling__window');
+
+    const render = () => {
+        verticalRollingWindow.appendChild(renderVerticalList(contentsData));
+
+        return verticalRollingWindow;
+    };
+
+    return render();
+};
