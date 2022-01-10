8ac602d93dcd45bb2e7ce489376a2f036723859e
terajh <terajoohyun@ajou.ac.kr>
2022-01-10 11:08:24 +0900

    feat: util selectorAll 함수 추가

--- src/public/scripts/utils/query.js
+++ src/public/scripts/utils/query.js
@@ -1,3 +1,7 @@
 export const $ = (query, parentNode = document) => {
-    return parentNode.querySelector(query);
+  return parentNode.querySelector(query);
+};
+
+export const $All = (query, parentNode = document) => {
+  return parentNode.querySelectorAll(query);
 };
