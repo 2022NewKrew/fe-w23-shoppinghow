06a29c7697571ecd32f36bf46cad052e61eef20e
fleek-moon <fleek.moon@kakaocorp.com>
2022-01-10 19:06:36 +0900

    feat : 최근 검색어 추가/삭제 기능 추가

--- client/src/components/header/search-container/index.js
+++ client/src/components/header/search-container/index.js
@@ -1,36 +1,64 @@
 import TextRoller from '@/components/common/text-roller';
+import HelpSearchContainer from '../help-search-container';
 import SearchForm from '../search-form';
 import './index.scss';
+import { getItemInLocalStroage, removeItemInLocalStroage } from '@/utils/local-storage';
 
 const cssNameForActivation = 'active';
 
 export default class SearchContainer {
+  state = {
+    searchName: getItemInLocalStroage('search-name'),
+  };
+
   constructor({ $parent }) {
     this.search = document.createElement('div');
     this.search.className = 'search';
     this.textRoller = new TextRoller({ $parent: this.search, onClick: this.activateInput.bind(this) });
-    this.searchForm = new SearchForm({ $parent: this.search });
+    this.searchForm = new SearchForm({ $parent: this.search, onSubmit: this.refreshSearchNameState.bind(this) });
+    this.helpSearchContainer = new HelpSearchContainer({
+      $parent: this.search,
+      onClick: this.handleClickHelpSearch.bind(this),
+    });
+
     $parent.appendChild(this.search);
 
     this.search.addEventListener('mouseleave', this.deactivateInput.bind(this));
+  }
+
+  refreshSearchNameState() {
+    this.state = { ...this.state, searchName: getItemInLocalStroage('search-name') };
+    this.setState(this.state);
+  }
+
+  handleClickHelpSearch(e) {
+    if (e.target.className === 'recent-search-name__remove') {
+      removeItemInLocalStroage('search-name', e.target.dataset.idx);
+      this.state = { ...this.state, searchName: getItemInLocalStroage('search-name') };
+      this.setState(this.state);
+    }
   }
 
   activateInput() {
     this.search.classList.add(cssNameForActivation);
     this.searchForm.focusInput();
     this.textRoller.deActivateRoller();
+    this.helpSearchContainer.activate();
   }
 
   deactivateInput() {
-    this.search.classList.contains(cssNameForActivation) && this.search.classList.remove(cssNameForActivation);
     if (!this.searchForm.getInputLength()) {
       this.searchForm.blurInput();
       this.textRoller.deActivateRoller();
       this.textRoller.activateRoller();
     }
+    this.search.classList.contains(cssNameForActivation) && this.search.classList.remove(cssNameForActivation);
+    this.helpSearchContainer.deActivate();
   }
 
   setState(props) {
-    this.textRoller.setState(props);
+    this.state = { ...props, ...this.state };
+    this.textRoller.setState(this.state);
+    this.helpSearchContainer.setState(this.state);
   }
 }
