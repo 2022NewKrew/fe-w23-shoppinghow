860d6eac911f59e100aebc472b1426afb60c5015
terajh <terajoohyun@ajou.ac.kr>
2022-01-10 11:09:43 +0900

    feat: api 서버 에러로직 추가 + category api 수정
    category 데이터를 top, middle, low 로 나누었습니다.

--- src/router/api.js
+++ src/router/api.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mysql = require("mysql2");
 const router = express.Router();
+const qs = require("querystring");
 
 const pool = mysql
   .createPool({
@@ -12,39 +13,71 @@ const pool = mysql
     connectionLimit: 10,
   })
   .promise();
+
 router.use("/", (req, res, next) => {
   req.url = decodeURIComponent(req.url);
   next();
 });
 
 router.get("/hotdeal", async (req, res) => {
-  const [result] = await pool.query(`select * from hotdeal`);
-  res.status(200).json({
-    result,
-  });
+  try {
+    const [result] = await pool.query(`select * from hotdeal`);
+    res.status(200).json({
+      result,
+    });
+  } catch (e) {
+    console.error(e);
+    res.status(404);
+  }
 });
 router.get("/event/top100", async (req, res) => {
-  const [result] = await pool.query(`select * from event where type = 2`);
-  res.status(200).json({
-    result,
-  });
+  try {
+    const [result] = await pool.query(`select * from event where type = 2`);
+    res.status(200).json({
+      result,
+    });
+  } catch (e) {
+    console.error(e);
+    res.status(404);
+  }
 });
 router.get("/event/slide", async (req, res) => {
-  const [result] = await pool.query(`select * from event where type = 1`);
-  res.status(200).json({
-    result,
-  });
+  try {
+    const [result] = await pool.query(`select * from event where type = 1`);
+    res.status(200).json({
+      result,
+    });
+  } catch (e) {
+    console.error(e);
+    res.status(404);
+  }
 });
 router.get("/event/items", async (req, res) => {
-  const [result] = await pool.query(`select * from event where type = 3`);
-  res.status(200).json({
-    result,
-  });
+  try {
+    const [result] = await pool.query(`select * from event where type = 3`);
+    res.status(200).json({
+      result,
+    });
+  } catch (e) {
+    console.error(e);
+    res.status(404);
+  }
 });
 router.get("/category", async (req, res) => {
-  const [result] = await pool.query(`select * from category`);
-  res.status(200).json({
-    result,
-  });
+  try {
+    const [result] = await pool.query(`select * from Category`);
+    const resData = {};
+    result.map(item => {
+      if (typeof resData[item.top] === "undefined") {
+        resData[item.top] = [{ middle: item.middle, low: item.low }];
+      } else {
+        resData[item.top].push({ middle: item.middle, low: item.low });
+      }
+    });
+    res.status(200).json({ result: resData });
+  } catch (e) {
+    console.error(e);
+    res.status(404);
+  }
 });
 module.exports = router;
