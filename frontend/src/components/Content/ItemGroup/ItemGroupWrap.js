ac47b54e997358f3bf09ed8f24feeee356f7ae04
rare.nick <rare.nick@kakaocorp.com>
2022-01-10 13:02:55 +0900

    fix: 콘텐츠에서 서버에서 데이터 받아오는 부분 분리 add: 핫딜 더미 아이템 추가 update: 콘텐츠와 핫딜아이템 폼 연결

--- frontend/src/components/Content/ItemGroup/ItemGroupWrap.js
+++ frontend/src/components/Content/ItemGroup/ItemGroupWrap.js
@@ -1,14 +1,37 @@
 import Component from '../../../core/Component';
-// TODO: cMain작업예정(상품 리스트)
+import {TARGET_SELECTOR, getTargetSelector} from '../../../util/ComponentGroup';
+import HotDealItemGroup from './HotDealItemGroup';
+
 export default class ItemGroupWrap extends Component {
   template() {
+    console.log(this.$props);
+    const itemGroupTitle = this.$props.title;
     return `
     <div class="tit_info">
-      <h3 class="tit_home"></h3>
+      <h3 class="tit_home">${itemGroupTitle}</h3>
     </div>
-    <div data-component="item-list" class="cont_item">
+    <div data-component="${TARGET_SELECTOR.ITEM_GROUP}" class="cont_item">
     </div>
     `;
+  }
+
+  mounted() {
+    this.setItemGroup();
+  }
+
+  setItemGroup() {
+    const itemType = this.$props.itemType;
+    // TODO: hotdeal,keyword 등 케이스 추가 예정
+    switch (itemType) {
+      case 'hotdeal':
+        this.setHotDealItemGroup();
+    }
+  }
+
+  setHotDealItemGroup() {
+    const itemGroup = this.$props.itemGroup;
+    const $hotDealItemGroup = this.$target.querySelector(getTargetSelector(TARGET_SELECTOR.ITEM_GROUP));
+    new HotDealItemGroup($hotDealItemGroup, {itemGroup: itemGroup});
   }
 }
 
