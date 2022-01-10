9ddcfed514171e1929f842ead93f85e9f25864d4
brad.ley <brad.ley@kakaocorp.com>
2022-01-10 21:29:57 +0900

    fix bug of carousel

--- src/javascripts/Components/Carousel.js
+++ src/javascripts/Components/Carousel.js
@@ -12,6 +12,7 @@ export default class Carousel extends Component {
     #bannerListEl
     
     #transitionDuration = 500
+    #bannerWidth = 500
     
     
     constructor(dataToShowList, option, bannerTemplate, drawBanner) {
@@ -34,35 +35,50 @@ export default class Carousel extends Component {
         this.#bannerListEl = this.rootEl.querySelector(`.${ styles.bannerList }`)
         this.#bannerListEl.addEventListener('transitionend', this.#processAfterTransition.bind(this))
         
-        const leftSideBtnEl = document.querySelector(styles.leftSideBtn)
-        const rightSideBtnEl = document.querySelector(styles.rightSideBtn)
+        const leftSideBtnEl = this.rootEl.querySelector(`.${ styles.leftSideBtn }`)
+        const rightSideBtnEl = this.rootEl.querySelector(`.${ styles.rightSideBtn }`)
         
         leftSideBtnEl.addEventListener('click', this.#moveLeft.bind(this))
-        leftSideBtnEl.addEventListener('click', this.#moveRight.bind(this))
+        rightSideBtnEl.addEventListener('click', this.#moveRight.bind(this))
+        
+        this.#bannerListEl.addEventListener('transitionend', this.#processAfterTransition.bind(this))
+        
+        this.update()
     }
     
     
     #applyOption(option) {
         const bannerContainerEl = this.rootEl.querySelector(`.${ styles.bannerContainer }`)
         
-        bannerContainerEl.style.width = option.bannerWidth
-        this.#transitionDuration = option.transitionDuration
+        if (option.bannerWidth) {
+            bannerContainerEl.style.width = option.bannerWidth
+            this.#bannerWidth = option.bannerWidth
+        }
+        
+        if (option.transitionDuration) {
+            this.#transitionDuration = option.transitionDuration
+            this.#bannerListEl.style.transitionDuration = `${ this.#transitionDuration }ms`
+        }
     }
     
     #moveLeft() {
-        this.#currentIdx--
-        this.#movePage()
+        if (this.#currentIdx > 0) {
+            this.#currentIdx--
+            this.#bannerListEl.style.transitionDuration = `${ this.#transitionDuration }ms`
+            this.#movePage()
+        }
     }
     
     #moveRight() {
-        this.#currentIdx++
-        this.#movePage()
+        if (this.#currentIdx < this.#dataToShowList.length + 1) {
+            this.#currentIdx++
+            this.#bannerListEl.style.transitionDuration = `${ this.#transitionDuration }ms`
+            this.#movePage()
+        }
     }
     
     #movePage() {
-        const bannerWidth = this.rootEl.style.width
-        
-        this.#bannerListEl.style.transform = `translateX(${ bannerWidth * this.#currentIdx })`
+        this.#bannerListEl.style.transform = `translateX(-${ this.#bannerWidth * this.#currentIdx }px)`
     }
     
     #processAfterTransition() {
@@ -81,24 +97,38 @@ export default class Carousel extends Component {
             }
             
             this.#movePage()
-            
-            this.#bannerListEl.style.transitionDuration = `${ this.#transitionDuration }ms`
         }
     }
     
+    #createBannerItemEl(dataToShow) {
+        const bannerItemEl = document.createElement('li')
+        bannerItemEl.classList.add(styles.bannerItem)
+        
+        bannerItemEl.innerHTML = this.#bannerTemplate
+        this.#drawBanner(bannerItemEl, dataToShow)
+        
+        return bannerItemEl
+    }
+    
     update() {
+        const bannerContainerEl = this.rootEl.querySelector(`.${ styles.bannerContainer }`)
+        const nTotalBanner = this.#dataToShowList.length + 2
+        const firstDataIdx = 0
+        const lastDataIdx = this.#dataToShowList.length - 1
+        
+        bannerContainerEl.style.width = `${ this.#bannerWidth }px`
+        
+        this.#bannerListEl.style.transitionDuration = `${ this.#transitionDuration }ms`
+        this.#bannerListEl.style.width = `${ this.#bannerWidth * nTotalBanner }px`
         this.#bannerListEl.innerHTML = ''
         
+        this.#bannerListEl.appendChild(this.#createBannerItemEl(this.#dataToShowList[lastDataIdx]))
         this.#dataToShowList.forEach((dataToShow) => {
-            const bannerItemEl = document.createElement('li')
-            
-            bannerItemEl.classList.add(styles.bannerItem)
-            bannerItemEl.innerHTML = this.#bannerTemplate
-            
-            this.#drawBanner(bannerItemEl, dataToShow)
-            
-            this.#bannerListEl.addChild(bannerItemEl)
+            this.#bannerListEl.appendChild(this.#createBannerItemEl(dataToShow))
         })
+        this.#bannerListEl.appendChild(this.#createBannerItemEl(this.#dataToShowList[firstDataIdx]))
+        
+        this.#movePage()
     }
     
     set dataToShowList(dataToShowList) {
@@ -107,3 +137,4 @@ export default class Carousel extends Component {
     }
     
 }
+
