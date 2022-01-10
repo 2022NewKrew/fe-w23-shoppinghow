8945b712736c4240f67494d643fd2574c6abd98f
fleek-moon <fleek.moon@kakaocorp.com>
2022-01-10 19:04:31 +0900

    feat : Add custom local-storage

--- client/src/utils/local-storage.js
+++ client/src/utils/local-storage.js
@@ -1,9 +1,17 @@
-export function getItem(key) {
+export function getItemInLocalStroage(key) {
   const value = localStorage.getItem(key);
-  return value ? JSON.parse(value) : null;
+  return value ? JSON.parse(value) : [];
 }
 
-export function setItem(key, value) {
-  const toJson = JSON.stringify(value);
-  localStorage.setItem(key, toJson);
+export function setItemInLocalStroage(key, inputValue, LIMIT) {
+  let currValue = getItemInLocalStroage(key);
+  currValue.push(inputValue);
+  currValue = currValue.length > LIMIT ? currValue.slice(1, currValue.length) : currValue;
+  localStorage.setItem(key, JSON.stringify(currValue));
+}
+
+export function removeItemInLocalStroage(key, index) {
+  const currValue = getItemInLocalStroage(key);
+  currValue.splice(index, 1);
+  localStorage.setItem(key, JSON.stringify(currValue));
 }
