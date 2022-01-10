9c7cc65c0a339562305c0773fed109ce4e2b594d
ethan.sglee <ethan.sglee@kakaocorp.com>
2022-01-10 10:56:37 +0900

    최근 본 상품 마우스오버 구현

--- ethan.sglee/shoppingHow/public/javascripts/recent.js
+++ ethan.sglee/shoppingHow/public/javascripts/recent.js
@@ -10,21 +10,37 @@ function appendRecentItem(parent, product) {
 }
 
 export default function initRecent() {
-
+  
   const recentBtn = document.querySelectorAll('.private-menu__btn')[1]
 
-  recentBtn.addEventListener('click', (event) => {
+  const recentWindow = document.querySelector('.private-menu__recent-window')
+  const recentContents = recentWindow.querySelector('.private-menu__recent-contents')
 
-    const recentContents = document.querySelector('.private-menu__recent-contents')
+  recentBtn.addEventListener('mouseenter', (event) => {
+    console.log('in')
+
+    recentWindow.classList.remove('none')
 
     recentContents.innerHTML = ''
-    
     for (let i=0; i<sessionStorage.length; i++) {
       let key = sessionStorage.key(i)
       const product = JSON.parse(sessionStorage.getItem(key))
       appendRecentItem(recentContents, product)
     }
 
+  })
+
+  recentBtn.addEventListener('mouseleave', (event) => {
+    console.log('out')
+    recentWindow.classList.add('none')
+  })
+
+  recentWindow.addEventListener('mouseenter', (event) => {
+    recentWindow.classList.remove('none')
+  })
+
+  recentWindow.addEventListener('mouseleave', (event) => {
+    recentWindow.classList.add('none')
   })
 
 }
