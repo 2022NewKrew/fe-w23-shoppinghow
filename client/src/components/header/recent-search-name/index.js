a9c24369928c8777f926dd39be30e0812e2930c4
fleek-moon <fleek.moon@kakaocorp.com>
2022-01-10 19:07:01 +0900

    feat : 최근 검색어 삭제위한 구조 수정

--- client/src/components/header/recent-search-name/index.js
+++ client/src/components/header/recent-search-name/index.js
@@ -4,27 +4,30 @@ import './index.scss';
 export default class RecentSearchName {
   recentSearchNameList = [];
 
-  constructor({ $parent, init }) {
+  constructor({ $parent, init, onClick }) {
     this.recentSearchName = document.createElement('div');
     this.recentSearchName.className = 'recent-search-name';
     this.recentSearchName.innerHTML = this.getFixedHTML();
     this.recentSearchUl = $('.recent-search-name__list', this.recentSearchName);
     this.recentSearchNameList = init;
+    this.onClick = onClick;
     this.render();
     $parent.appendChild(this.recentSearchName);
+
+    this.recentSearchName.addEventListener('click', this.onClick);
   }
 
   setState(props) {
-    this.recentSearchNameList = props;
+    this.recentSearchNameList = props.searchName;
     this.render();
   }
 
   render() {
     this.recentSearchUl.innerHTML = this.recentSearchNameList
-      .map((name) => {
+      .map((name, index) => {
         return `<li class='recent-search-name__item'>
                   <span class='recent-search-name__item-name'>${name}</span>
-                  <span class="recent-search-name__remove">❌</span>
+                  <span class="recent-search-name__remove" data-idx=${index}>❌</span>
                 </li>`;
       })
       .join('');
