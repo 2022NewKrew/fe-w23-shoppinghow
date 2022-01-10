6224c9910b5989a56bc57308fb5db5c95d323605
rare.nick <rare.nick@kakaocorp.com>
2022-01-10 14:20:23 +0900

    update: 배너, 키워드부분 임시데이터로 ui구현(임시데이터는 핫딜과 동일 변경예정)

--- frontend/src/components/Content/Banner/Banner.js
+++ frontend/src/components/Content/Banner/Banner.js
@@ -1,7 +1,7 @@
 import Component from '../../../core/Component';
 import {TARGET_SELECTOR, getTargetSelector} from '../../../util/ComponentGroup';
 import SlideBanner from './SlideBanner';
-
+import HotDealItemGroup from '../ItemGroup/HotDealItemGroup';
 export default class Banner extends Component {
   template() {
     const bannerData = this.$props.bannerData;
@@ -17,7 +17,7 @@ export default class Banner extends Component {
         <div data-component="${TARGET_SELECTOR.SLIDE_BANNER}" class="#banner_mileage evt_slide _GL">
         </div>
     </div>
-    <div data-component="${TARGET_SELECTOR.MALL_EVENT_LIST}" id="mallEventList" class="#banner_mallevent cont_item cont_top _GL">
+    <div data-component="${TARGET_SELECTOR.ITEM_GROUP}" class="cont_item">
     </div>
     `;
   }
@@ -25,5 +25,19 @@ export default class Banner extends Component {
   mounted() {
     const $slideBanner = this.$target.querySelector(getTargetSelector(TARGET_SELECTOR.SLIDE_BANNER));
     new SlideBanner($slideBanner, {bannerData: this.$props.bannerData.special});
+
+    this.setHotDealForm();
+  }
+
+  // TODO 서버에서 데이터 받아올 얘정 지금은 임시 데이터
+  async setHotDealForm() {
+    const tempData = Array(4).fill({
+      category: '호빵찜기',
+      name: 'JY-507 호빵찜기 소형',
+      imgUrl: 'asset/img/product_hotdeal/product_hotdeal_01.jpeg',
+      price: 705150,
+    });
+    const $hotDealItemGroup = this.$target.querySelector(getTargetSelector(TARGET_SELECTOR.ITEM_GROUP));
+    new HotDealItemGroup($hotDealItemGroup, {itemGroup: tempData});
   }
 }
