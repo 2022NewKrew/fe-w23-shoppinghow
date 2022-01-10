e273a1a9a723a1aec79c888a049d3b2477ca5fec
rare.nick <rare.nick@kakaocorp.com>
2022-01-10 18:15:23 +0900

    add: 핫 키워드 폼 추가, 더미데이터 추가 update: 기존 핫딜 폼 키워드 폼으로 변경

--- frontend/src/components/Content/ItemGroup/HotDealItemGroup.js
+++ frontend/src/components/Content/ItemGroup/HotDealItemGroup.js
@@ -4,18 +4,27 @@ export default class HotDealItemGroup extends Component {
   template() {
     const itemGroup = this.$props.itemGroup;
     const itemForm = (itemInfo, index) =>{
-      return `<li class="_GI_">
-        <strong class="tit_rank">
-            <a href="" class="_GC_">${index} . ${itemInfo.category}</a>
-        </strong>
-        <a href="/search/키보드/" class="link_prod _GC_" style="display: inline-block;">
-            <img src="${itemInfo.imgUrl}" width="200" height="200" class="img_g">
-            <strong class="tit_g">${itemInfo.name}</strong>
-            <span class="txt_desc">
-                <span class="num_price">${itemInfo.price}</span>원
+      const itemName = itemInfo.name;
+      const itemDiscountPrice = itemInfo.discountPrice;
+      const itemDiscountPercent = itemInfo.discountPercent;
+      const itemRegularPrice = itemInfo.regularPrice;
+      const itemImgUrl = itemInfo.imgUrl;
+      return `
+      <li class="_GI_">
+        <a href="" class="link_prod _GC_">
+          <span class="thumb_hotdeal">
+            <img src="${itemImgUrl}" class="img_g" alt="">
+          </span>
+          <strong class="tit_g">${itemName}</strong>
+          <span class="detail_price">
+            <span class="info_discount">
+              <span class="txt_discount">${itemDiscountPrice}<span class="txt_unit">원</span></span>
+              <span class="txt_percent">${itemDiscountPercent}<span class="txt_unit">%</span></span>
             </span>
+            <span class="txt_price">${itemRegularPrice}<span class="screen_out">원</span></span>
+          </span>
         </a>
-    </li>`;
+      </li>`;
     };
     return `
     <ul class="list_item _GL" data-gg="{lk:pctop_reckword_c}">
