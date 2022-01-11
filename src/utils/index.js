3d217ab5720c93bb713a5ce7d79bbc3b934d0f9b
miro-ring <miro.ring@kakaocorp.com>
2022-01-11 14:40:13 +0900

    feat: 헬퍼 함수들 추가(중복 코드 제거를 위한)

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,16 @@
-const $ = (selector) => document.querySelector(selector);
-const $$ = (selector) => document.querySelectorAll(selector);
+export const $ = (selector) => document.querySelector(selector);
 
-export { $, $$ }
+export const $$ = (selector) => document.querySelectorAll(selector);
+
+export const setMVC = async (Model, View, Controller) => {
+    const model = new Model();
+    await model.init();
+    const view = new View(model);
+    const controller = new Controller(model, view);
+    controller.render();
+};
+
+export const mount = (target, templateID) => {
+    const template = $(templateID).content.children[0];
+    target.appendChild(template.cloneNode(true));
+};
