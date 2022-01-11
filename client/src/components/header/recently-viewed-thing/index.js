5f576d5fe7ae0c8b3b716e59d726637478f016b1
fleek-moon <fleek.moon@kakaocorp.com>
2022-01-11 12:33:51 +0900

    feat : 최근본상품 추가/삭제 기능 추가

--- client/src/components/header/recently-viewed-thing/index.js
+++ client/src/components/header/recently-viewed-thing/index.js
@@ -1,18 +1,24 @@
+import './index.scss';
 import { LOGIN_ICON, RECENTLY_VIEWED_THINGS_ICON } from '@/static/constants/image-path';
 import { $ } from '@/utils/helper';
-import './index.scss';
+import { getItemInLocalStroage, setItemInLocalStroage, removeItemInLocalStroage } from '@/utils/local-storage';
 
-const enableModalTime = 200;
-const disableModalTime = 2000;
+const ENABLE_MODAL_TIME = 200;
+const DISABLE_MODAL_TIME = 2000;
+const RECENTLY_VIEWED_THING_IMG_LIMIT = 6;
 
 // RVT : RecentlyViewedThing
 export default class RecentlyViewedThing {
   rvtTimer = 0;
   modalTimer = 0;
+  recentlyViewedThingImgs = getItemInLocalStroage('recently-viewed-thing');
+
   constructor({ $parent }) {
     this.recentlyViewedThing = document.createElement('div');
     this.recentlyViewedThing.className = 'recently-viewed-thing';
     this.recentlyViewedThing.innerHTML = this.createInitView();
+    this.modalContent = $('.modal-main', this.recentlyViewedThing);
+
     this.render();
     $parent.appendChild(this.recentlyViewedThing);
 
@@ -21,16 +27,37 @@ export default class RecentlyViewedThing {
     this.recentlyViewedThing.addEventListener('mouseleave', this.handleMouseLeaveRVT.bind(this));
     this.modal.addEventListener('mouseenter', this.handleMouseEnterModal.bind(this));
     this.modal.addEventListener('mouseleave', this.handleMouseLeaveModal.bind(this));
+    document.body.addEventListener('addToRecentlyViewedThing', this.handleCustomEventBody.bind(this));
   }
 
-  setState(props) {}
+  handleCustomEventBody(e) {
+    this.removeDulication(e);
 
-  render() {}
+    setItemInLocalStroage('recently-viewed-thing', e.detail.imageSrc, RECENTLY_VIEWED_THING_IMG_LIMIT);
+    const newState = getItemInLocalStroage('recently-viewed-thing');
+    this.setState(newState);
+  }
+
+  removeDulication(e) {
+    const duplicationIndex = this.recentlyViewedThingImgs.findIndex((img) => img === e.detail.imageSrc);
+    if (duplicationIndex >= 0) removeItemInLocalStroage('recently-viewed-thing', duplicationIndex);
+  }
+
+  setState(props) {
+    this.recentlyViewedThingImgs = props;
+    this.render();
+  }
+
+  render() {
+    this.modalContent.innerHTML = this.recentlyViewedThingImgs.length
+      ? this.recentlyViewedThingImgs.map((imgSrc) => `<img src=${imgSrc}>`).join('')
+      : `<div class="modal-no-item"><span class="modal-highlight-span">최근 본 상품</span>이 없습니다.</div>`;
+  }
 
   handleMouseEnterRVT() {
     this.timer = setTimeout(() => {
       this.modal.style.display = 'flex';
-    }, enableModalTime);
+    }, ENABLE_MODAL_TIME);
   }
 
   handleMouseLeaveRVT() {
@@ -44,7 +71,7 @@ export default class RecentlyViewedThing {
   handleMouseLeaveModal() {
     this.modalTimer = setTimeout(() => {
       this.modal.style.display = 'none';
-    }, disableModalTime);
+    }, DISABLE_MODAL_TIME);
   }
 
   createInitView() {
@@ -53,7 +80,8 @@ export default class RecentlyViewedThing {
         <span>최근본 상품</span>
         <div class="modal">
             <h3 class="modal-title">최근 본 상품</h3>
-            <div class="modal-main"><div class="modal-no-item"><span class="modal-highlight-span">최근 본 상품</span>이 없습니다.</div></div>
+            <hr>
+            <div class="modal-main"></div>
             <div class="modal-description">
                 <span>로그인 하시면 더 많은<br> MY 쇼핑 정보를 확인 하실 수 있습니다</span>
                 <div class="modal-login"> 
