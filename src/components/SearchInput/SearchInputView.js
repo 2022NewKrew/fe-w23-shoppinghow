3dfc357770dd56e704f0cf88e28b9c65051b5369
miro-ring <miro.ring@kakaocorp.com>
2022-01-11 14:50:28 +0900

    검색박스 구현

---
+++ src/components/SearchInput/SearchInputView.js
@@ -0,0 +1,10 @@
+import { View } from "@core";
+
+export class SearchInputView extends View {
+    
+    createSearchRanking(ranks) {
+        return ranks
+            .map((rank, idx) => `<li class="search-top10__item">${idx === 10 ? 1 : idx + 1}. ${rank}</li>`)
+            .join("");
+    }
+}
