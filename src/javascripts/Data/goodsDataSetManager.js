0d2e4c185d7dca75774ed5cb1f624d29a4392ece
brad.ley <brad.ley@kakaocorp.com>
2022-01-11 16:03:41 +0900

    create a GoodsDataSetManager class to delegate the role of GoodsDataManager by dividing it into recently viewed products and favorite products

---
+++ src/javascripts/Data/goodsDataSetManager.js
@@ -0,0 +1,18 @@
+import DataSetManager from '../Core/DataSetManager'
+
+class GoodsDataSetManager extends DataSetManager {
+    
+    constructor(localStorageKey) {
+        super(localStorageKey)
+    }
+    
+    checkIfSame(data1, data2) {
+        return data1.title === data2.title
+            && data1.imgSrc === data2.imgSrc
+            && data1.price === data2.price
+    }
+    
+}
+
+export const recentlyViewedGoodsDataSetManager = new GoodsDataSetManager('recentlyViewedGoodsData')
+export const taggedGoodsDataSetManager = new GoodsDataSetManager('taggedGoodsData')
