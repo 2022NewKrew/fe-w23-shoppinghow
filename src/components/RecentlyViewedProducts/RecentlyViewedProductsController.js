e3264ef92ca0df9d3b5d62ad1821a8bd52e8e69b
miro-ring <miro.ring@kakaocorp.com>
2022-01-11 14:50:11 +0900

    feat: 최근 본 상품 박스 구현(미완성)

---
+++ src/components/RecentlyViewedProducts/RecentlyViewedProductsController.js
@@ -0,0 +1,25 @@
+import { Controller } from "@core";
+
+export class RecentlyViewedProductsController extends Controller {
+
+    init() {
+        const recentlyViewedProductsListDOM = this.view.createRecentlyViewedProducts();
+        this.view.mount({ 
+            parentClassName: "private-menu__btn.recently_viewed", 
+            childNode: recentlyViewedProductsListDOM 
+        });
+    }
+
+    setEvent() {
+        const recentlyViewed = this.view.getDOMByClassName("private-menu__btn.recently_viewed");
+        const recentlyViewedList = this.view.getDOMByClassName("rvpv_products_wrap");
+        
+        recentlyViewed.addEventListener("mouseover", () => {
+            recentlyViewedList.style.display = "block"
+        });
+
+        recentlyViewed.addEventListener("mouseout", () => {
+            recentlyViewedList.style.display = "none"
+        });
+    }
+}
---
+++ src/components/RecentlyViewedProducts/RecentlyViewedProductsModel.js
@@ -0,0 +1,6 @@
+export class RecentlyViewedProductsModel {
+    // TODO: 데이터 설정 필요
+    constructor() {}
+
+    init() {}
+}
