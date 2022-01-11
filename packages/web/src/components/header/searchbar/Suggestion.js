473643d8fb861349f944aff59b838d63da3c7963
ethan-3160 <ethan.3160@kakaocorp.com>
2022-01-11 14:06:08 +0900

    Add: 검색바 최근검색어

--- packages/web/src/components/header/searchbar/Suggestion.js
+++ packages/web/src/components/header/searchbar/Suggestion.js
@@ -13,6 +13,14 @@ const sample = [
   "핸드크림",
 ];
 
+const rcntSample = [
+  "만년필",
+  "대리석식탁",
+  "석류즙",
+  "캡슐커피",
+  "황사마스크kf94",
+];
+
 export default class Suggestion {
   constructor({ $app, initialState }) {
     this.state = initialState;
@@ -25,31 +33,45 @@ export default class Suggestion {
   render() {
     this.$target.innerHTML = `
         <div class="inner_suggestion">
-            <strong class="tit_suggestion">인기 쇼핑 키워드</strong>
-            <ol class="list_keyword">
-                ${sample
-                  .slice(0, 5)
-                  .map(
-                    (word, idx) => `
-                <li><a href="#" class="keyword"><span class="num_rank">${
-                  idx + 1
-                }</span>${word}</a></li>
-                `
-                  )
-                  .join("")}
-            </ol>
-            <ol class="list_keyword">
-            ${sample
-              .slice(5)
-              .map(
-                (word, idx) => `
-              <li><a href="#" class="keyword"><span class="num_rank">${
-                idx + 6
-              }</span>${word}</a></li>
-              `
-              )
-              .join("")}</ol>
+            <div class="rcnt">
+                <strong class="tit_suggestion">최근 검색어</strong>
+                <ol class="list_keyword" id="rcnt">
+                    ${this.createListRcnt(rcntSample)}
+                </ol>
+            </div>
+            <div class="pop">
+                <strong class="tit_suggestion">인기 쇼핑 키워드</strong>
+                <ol class="list_keyword">
+                    ${this.createListKeyword(sample, 0, 5)}
+                </ol>
+                <ol class="list_keyword">
+                    ${this.createListKeyword(sample, 5, 10)}
+                </ol>
+            </div>
+            
         </div>
         `;
+  }
+  createListRcnt(data) {
+    return data
+      .map(
+        (word) =>
+          `<li><a href="#" class="keyword">${word}<span class="ico_remove"></span></a></li>`
+      )
+      .join("");
+  }
+
+  createListKeyword(data, start, end) {
+    return data
+      .slice(start, end)
+      .map(
+        (word, idx) =>
+          `
+        <li><a href="#" class="keyword"><span class="num_rank">${
+          idx + start + 1
+        }</span>${word}</a></li>
+        `
+      )
+      .join("");
   }
 }
