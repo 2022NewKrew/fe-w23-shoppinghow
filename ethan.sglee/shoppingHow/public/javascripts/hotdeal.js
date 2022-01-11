1e1c18aae7fdc590016be647fe4769d721c8f727
ethan.sglee <ethan.sglee@kakaocorp.com>
2022-01-11 13:54:09 +0900

    찜하기 구현

--- ethan.sglee/shoppingHow/public/javascripts/hotdeal.js
+++ ethan.sglee/shoppingHow/public/javascripts/hotdeal.js
@@ -1,13 +1,29 @@
+const EMPTY_HEART_TEMPLATE = `
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+  <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+</svg>
+`
+const FILL_HEART_TEMPLATE = `
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+</svg>
+`
+
 function addHotDealHTML(imgSrc, idx) {
   const target = document.querySelector(".hot-deal-list")
-  const hotdealItemTpl = `
+  const hotdealItemTpl = 
+  `
   <li class="hot-deal__item">
       <a href="#" class="hot-deal__link">
           <span class="hot-deal__thumb">
               <img src="${imgSrc}" class="hot-deal__img" alt="">
           </span>
-
-          <strong class="hot-deal__title">${idx}번째 상품</strong>
+          <div class="hot-deal__title-bar">
+            <strong class="hot-deal__title">${idx}번째 상품</strong>
+            <div class="hot-deal__favorite-icon">
+              ${EMPTY_HEART_TEMPLATE}
+            </div>
+          </div>
 
           <span class="hot-deal__detail-price">
               <span class="txt-price">${idx + 1},500 </span>
@@ -39,11 +55,29 @@ export default function initHotdeal() {
   })
 
   const hotdealList = document.querySelector(".hot-deal-list")
+  
+  hotdealList.querySelectorAll('.hot-deal__item').forEach(item => {
+    const title = item.querySelector('.hot-deal__title').innerText
+    if (localStorage.getItem(title) !== null) {
+      const heart = item.querySelector('.hot-deal__favorite-icon')
+      heart.innerHTML = FILL_HEART_TEMPLATE
+    }
+  })
 
   hotdealList.addEventListener('click', (event) => {
-    const imgSrc = event.target.querySelector('.hot-deal__thumb > img').src
-    const title = event.target.querySelector('.hot-deal__title').innerText
-    const price = event.target.querySelector('.txt-price').innerText
+    event.preventDefault()
+
+    const path = event.composedPath()
+    const currentItem = path.filter(tag => {
+      if ('classList' in tag) {
+        return tag.classList.contains('hot-deal__item')
+      }
+      return false
+    })[0]
+
+    const imgSrc = currentItem.querySelector('.hot-deal__thumb > img').src
+    const title = currentItem.querySelector('.hot-deal__title').innerText
+    const price = currentItem.querySelector('.txt-price').innerText
 
     const product = {
       imgSrc,
@@ -51,7 +85,17 @@ export default function initHotdeal() {
       price,
     }
 
-    sessionStorage.setItem(title, JSON.stringify(product))
+    if (event.target.classList.contains('hot-deal__favorite-icon')) {
+      if (localStorage.getItem(title) !== null) {
+        localStorage.removeItem(title)
+        event.target.innerHTML = EMPTY_HEART_TEMPLATE
+      } else {
+        localStorage.setItem(title, JSON.stringify(product))
+        event.target.innerHTML = FILL_HEART_TEMPLATE
+      }
+    } else {
+      sessionStorage.setItem(title, JSON.stringify(product))
+    }
   })
 }
 
--- ethan.sglee/shoppingHow/public/javascripts/recent.js
+++ ethan.sglee/shoppingHow/public/javascripts/recent.js
@@ -1,3 +1,6 @@
+const RECENT_PRODUCT_TITLE = '최근 본 상품'
+const FAVORITE_PRODUCT_TITLE = '내가 찜한 상품'
+
 function appendRecentItem(parent, product) {
   const recentItemTpl = `
   <li class="private-menu__recent-contents__item">
@@ -12,19 +15,32 @@ function appendRecentItem(parent, product) {
 
 export default function initRecent() {
   const recentBtn = document.querySelectorAll('.private-menu__btn')[1]
-
+  
   const recentWindow = document.querySelector('.private-menu__recent-window')
   const recentContents = recentWindow.querySelector('.private-menu__recent-contents')
+  
+  function refreshContents(contents_title) {
+    recentContents.innerHTML = ''
+  
+    if (contents_title === RECENT_PRODUCT_TITLE) {
+      for (let i=0; i<sessionStorage.length; i++) {
+        let key = sessionStorage.key(i)
+        const product = JSON.parse(sessionStorage.getItem(key))
+        appendRecentItem(recentContents, product)
+      }
+    } else if (contents_title === FAVORITE_PRODUCT_TITLE) {
+      for (let i=0; i<localStorage.length; i++) {
+        let key = localStorage.key(i)
+        const product = JSON.parse(localStorage.getItem(key))
+        appendRecentItem(recentContents, product)
+      }
+    }
+  }
 
   recentBtn.addEventListener('mouseenter', () => {
     recentWindow.classList.remove('none')
 
-    recentContents.innerHTML = ''
-    for (let i=0; i<sessionStorage.length; i++) {
-      let key = sessionStorage.key(i)
-      const product = JSON.parse(sessionStorage.getItem(key))
-      appendRecentItem(recentContents, product)
-    }
+    refreshContents(RECENT_PRODUCT_TITLE)
   })
 
   recentBtn.addEventListener('mouseleave', () => {
@@ -37,6 +53,15 @@ export default function initRecent() {
 
   recentWindow.addEventListener('mouseleave', () => {
     recentWindow.classList.add('none')
+  })
+
+  recentWindow.addEventListener('click', (event) => {
+    console.log(event.target.innerText)
+    if (event.target.innerText === RECENT_PRODUCT_TITLE) {
+      refreshContents(RECENT_PRODUCT_TITLE)
+    } else if (event.target.innerText === FAVORITE_PRODUCT_TITLE) {
+      refreshContents(FAVORITE_PRODUCT_TITLE)
+    }
   })
 
 }
