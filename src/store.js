8a353bb5e813a078382213cbf8d7da66bf104b5b
Remian103 <alsdnska@naver.com>
2022-01-11 18:11:43 +0900

    store.js: 오타 수정 및 state value 추가

--- src/store.js
+++ src/store.js
@@ -6,18 +6,22 @@ class Store {
     constructor(initState) {
         this.state = observable({
             ...initState
-        })
+        });
     }
 
     setState(newState) { // 해당하는 키값만 변경
-        for (const [key, value] of Objects.entries(newState)) {
+        for (const [key, value] of Object.entries(newState)) {
             if(!this.state[key]) continue;
             this.state[key] = value;
         }
     }
 }
 
-export const store = new store({
+export const store = new Store({
     top10: [],
     recentWord: [],
+    menu: {
+        topMenuList: [],
+        keywordList: [],
+    }
 })
