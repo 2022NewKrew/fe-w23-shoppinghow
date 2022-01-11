c71fd9db6faf8b06fe66ed880bf7e1396e2faee4
fleek-moon <fleek.moon@kakaocorp.com>
2022-01-11 15:03:55 +0900

    feat : 최근 본 상품 갯수 UI 동적렌더링

--- client/src/components/header/recently-viewed-thing/index.js
+++ client/src/components/header/recently-viewed-thing/index.js
@@ -18,11 +18,12 @@ export default class RecentlyViewedThing {
     this.recentlyViewedThing.className = 'recently-viewed-thing';
     this.recentlyViewedThing.innerHTML = this.createInitView();
     this.modalContent = $('.modal-main', this.recentlyViewedThing);
-
+    this.modalThingsLength = $('.modal-things-length', this.recentlyViewedThing);
     this.render();
     $parent.appendChild(this.recentlyViewedThing);
 
     this.modal = $('.modal', this.recentlyViewedThing);
+
     this.recentlyViewedThing.addEventListener('mouseenter', this.handleMouseEnterRVT.bind(this));
     this.recentlyViewedThing.addEventListener('mouseleave', this.handleMouseLeaveRVT.bind(this));
     this.modal.addEventListener('mouseenter', this.handleMouseEnterModal.bind(this));
@@ -52,6 +53,8 @@ export default class RecentlyViewedThing {
     this.modalContent.innerHTML = this.recentlyViewedThingImgs.length
       ? this.recentlyViewedThingImgs.map((imgSrc) => `<img src=${imgSrc}>`).join('')
       : `<div class="modal-no-item"><span class="modal-highlight-span">최근 본 상품</span>이 없습니다.</div>`;
+
+    this.modalThingsLength.innerHTML = this.recentlyViewedThingImgs.length;
   }
 
   handleMouseEnterRVT() {
@@ -79,7 +82,7 @@ export default class RecentlyViewedThing {
         <img class="recently-viewed-thing-img" src=${RECENTLY_VIEWED_THINGS_ICON} alt='최근 본 상품 아이콘'/>
         <span>최근본 상품</span>
         <div class="modal">
-            <h3 class="modal-title">최근 본 상품</h3>
+            <h3 class="modal-title">최근 본 상품 <span class="modal-things-length"></span></h3>
             <hr>
             <div class="modal-main"></div>
             <div class="modal-description">
