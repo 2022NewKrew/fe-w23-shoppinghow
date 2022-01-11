f554ad168ea2e7e8ed79bb370b765d7d10559c0d
fleek-moon <fleek.moon@kakaocorp.com>
2022-01-11 17:24:27 +0900

    feat : Use rxjs for auto complete

--- client/src/components/header/search-form/index.js
+++ client/src/components/header/search-form/index.js
@@ -1,13 +1,16 @@
 import './index.scss';
+import { fromEvent } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, map, switchMap } from 'rxjs/operators';
 import { $ } from '@/utils/helper';
 import { SEARCH_ICON } from '@/static/constants/image-path';
-import { setItemInLocalStroage } from '../../../utils/local-storage';
+import { setItemInLocalStroage } from '@/utils/local-storage';
+import { api } from '@/api';
 
 const SEARCH_NAME_LENGTH_LIMIT = 5;
 const INPUT_MAX_LENGTH = 15;
 
 export default class SearchForm {
-  constructor({ $parent, onSubmit }) {
+  constructor({ $parent, onSubmit, refreshAutoCompleteWord, refreshInputValue }) {
     this.searchForm = document.createElement('form');
     $parent.appendChild(this.searchForm);
     this.render();
@@ -16,6 +19,24 @@ export default class SearchForm {
     this.searchInput = $('.search__input', this.searchForm);
     this.searchForm.addEventListener('click', this.handleClickSearchForm.bind(this));
     this.searchForm.addEventListener('submit', this.handleSubmitSearchForm.bind(this));
+
+    fromEvent(this.searchInput, 'input')
+      .pipe(
+        map((e) => e.target.value),
+        filter((text) => text.length > 0),
+        debounceTime(500),
+        distinctUntilChanged(),
+        switchMap((searchTerm) => api.get(`/item/autocomplete?search=${searchTerm}`)),
+        map((res) => res.result)
+      )
+      .subscribe((searchTerm) => {
+        const titles = searchTerm.map((term) => term.title);
+        refreshAutoCompleteWord(titles);
+      });
+
+    fromEvent(this.searchInput, 'input')
+      .pipe(map((e) => e.target.value))
+      .subscribe((input) => refreshInputValue(input));
   }
 
   render() {
