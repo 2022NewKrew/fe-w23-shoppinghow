4056c22ffae9e522dca9e55577016ce54f9ae066
fleek-moon <fleek.moon@kakaocorp.com>
2022-01-11 17:23:14 +0900

    feat : Add AutoComplete Component

---
+++ client/src/components/header/auto-complete/index.js
@@ -0,0 +1,36 @@
+import './index.scss';
+
+export default class AutoComplete {
+  words = [];
+
+  constructor({ $parent }) {
+    this.autoComplete = document.createElement('ul');
+    this.autoComplete.className = 'autocomplete__list';
+
+    this.render();
+    $parent.appendChild(this.autoComplete);
+  }
+
+  setState(props) {
+    this.words = [...props.autoCompleteWords];
+    this.render();
+  }
+
+  render() {
+    this.autoComplete.innerHTML = this.words
+      .map((name, index) => {
+        return `<li class='autocomplete__item'>
+                    <span class='autocomplete__word'>${name}</span>
+                </li>`;
+      })
+      .join('');
+  }
+
+  activate() {
+    this.autoComplete.style.display = 'flex';
+  }
+
+  deActivate() {
+    this.autoComplete.style.display = 'none';
+  }
+}
