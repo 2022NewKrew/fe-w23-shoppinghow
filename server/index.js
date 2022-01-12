017539b13e8a5807e194b8beaeb4fd67e0d1f2d8
vvvpiano <vvvpiano60@gmail.com>
2022-01-12 17:58:19 +0900

    feat: json-server 셋팅 후 server와 연결

--- server/index.js
+++ server/index.js
@@ -1,26 +1,29 @@
-const express = require("express");
-const webpack = require("webpack");
-const webpackDevMiddleWare = require("webpack-dev-middleware");
-const path = require("path");
+const express = require('express');
+const webpack = require('webpack');
+const webpackDevMiddleWare = require('webpack-dev-middleware');
+const fetch = require('node-fetch');
+const path = require('path');
+require('dotenv').config();
 
 const app = express();
-const config = require("../webpack.config.js");
+const config = require('../webpack.config.js');
 const compiler = webpack(config);
 const PORT = process.env.PORT || 8080;
+const DB_URL = process.env.DB_BASE_URL;
 
 const __rootDir = path.resolve();
-app.use(express.static("../dist"));
+app.use(express.static('../dist'));
 
 app.use(
     webpackDevMiddleWare(compiler, {
-        publicPath: config.output.publicPath
+        publicPath: config.output.publicPath,
     })
-)
+);
 
-app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__rootDir, "dist", "index.html"));
-})
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__rootDir, 'dist', 'index.html'));
+});
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
     console.log(`Server is listening on port: ${PORT}`);
-})
+});
