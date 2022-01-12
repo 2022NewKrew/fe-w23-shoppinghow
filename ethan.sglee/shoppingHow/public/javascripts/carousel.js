a076f49b918c95c73a90f9b6222fac3390d38f88
ethan.sglee <ethan.sglee@kakaocorp.com>
2022-01-13 01:20:50 +0900

    캐러셀 구현 개선

--- ethan.sglee/shoppingHow/public/javascripts/carousel.js
+++ ethan.sglee/shoppingHow/public/javascripts/carousel.js
@@ -1,67 +1,90 @@
+
 export default function initCarousel () {
   const planningWindow = document.querySelector('.planning__window')
+  
+  function getTranslate3dText(pageNum) {
+    return 'translate3d(' + -(pageNum * planningWindow.clientWidth) + 'px,0px,0px)'
+  }
 
   const linkContainer = planningWindow.querySelector('.planning__link-container')
   const btnContainer = planningWindow.querySelector('.planning__btn-container')
   const paging = planningWindow.querySelector('.planning__paging')
+  const pageArr = paging.querySelectorAll('.planning__page')
+  const PAGE_MAX_NUM = pageArr.length
 
   const leftBtn = btnContainer.querySelector('.planning__left-btn')
   const rightBtn = btnContainer.querySelector('.planning__right-btn')
 
+  const firstPageCopy = linkContainer.firstElementChild.cloneNode(true)
+  const lastPageCopy = linkContainer.lastElementChild.cloneNode(true)
+  linkContainer.insertBefore(lastPageCopy, linkContainer.firstElementChild)
+  linkContainer.appendChild(firstPageCopy)
+
+  let currentPageNum = 0
   let direction = ''
   let ongoing = false
 
-  btnContainer.addEventListener('click', ({target}) => {
+  leftBtn.addEventListener('click', () => {
     if (ongoing) {
       return
     }
+
+    ongoing = true
+
+    const current = paging.querySelector('.planning__current')
+    current.classList.remove('planning__current')
+
+    currentPageNum -= 1
     
-    if (target === leftBtn) {
-      ongoing = true
-
-      const current = paging.querySelector('.planning__current')
-
-      if (!current.parentElement.previousElementSibling) {
-        paging.lastElementChild.firstElementChild.classList.add('planning__current')
-      } else {
-        current.parentElement.previousElementSibling.firstElementChild.classList.add('planning__current')
-      }
-
-      current.classList.remove('planning__current')
-
-      direction = 'left'
-      linkContainer.style.transition = 'transform 0.3s ease-in-out'
-      linkContainer.style.transform = 'translate3d(' + planningWindow.clientWidth + 'px,0px,0px)'
-    } else if (target === rightBtn) {
-      ongoing = true
-
-      const current = paging.querySelector('.planning__current')
-
-      if (current.parentElement.nextElementSibling === null) {
-        paging.firstElementChild.firstElementChild.classList.add('planning__current')
-      } else {
-        current.parentElement.nextElementSibling.firstElementChild.classList.add('planning__current')
-      }
-
-      current.classList.remove('planning__current')
-
-      direction = 'right'
-      linkContainer.style.transition = 'transform 0.3s ease-in-out'
-      linkContainer.style.transform = 'translate3d(' + -planningWindow.clientWidth + 'px,0px,0px)'
+    if (currentPageNum < 0) {
+      pageArr[PAGE_MAX_NUM - 1].querySelector('.planning__page-btn').classList.add('planning__current')
+    } else {
+      pageArr[currentPageNum].querySelector('.planning__page-btn').classList.add('planning__current')
     }
 
+    direction = 'left'
+    linkContainer.style.transition = 'transform 0.3s ease-in-out'
+    linkContainer.style.transform = getTranslate3dText(currentPageNum)
+  })
+
+  rightBtn.addEventListener('click', () => {
+    if (ongoing) {
+      return
+    }
+
+    ongoing = true
+
+    const current = paging.querySelector('.planning__current')
+    current.classList.remove('planning__current')
+
+    currentPageNum += 1
+    
+    if (currentPageNum >= PAGE_MAX_NUM) {
+      pageArr[0].querySelector('.planning__page-btn').classList.add('planning__current')
+    } else {
+      pageArr[currentPageNum].querySelector('.planning__page-btn').classList.add('planning__current')
+    }
+
+    direction = 'right'
+    linkContainer.style.transition = 'transform 0.3s ease-in-out'
+    linkContainer.style.transform = getTranslate3dText(currentPageNum)
   })
 
   linkContainer.addEventListener('transitionend', () => {
     if (direction === 'left') {
-      linkContainer.insertBefore(linkContainer.lastElementChild, linkContainer.firstElementChild)
+      if (currentPageNum < 0) {
+        currentPageNum = PAGE_MAX_NUM - 1
+        linkContainer.style.transition = ''
+        linkContainer.style.transform = getTranslate3dText(PAGE_MAX_NUM - 1)
+      }
     }
     else if (direction === 'right') {
-      linkContainer.appendChild(linkContainer.firstElementChild)
+      if (currentPageNum >= PAGE_MAX_NUM) {
+        currentPageNum = 0
+        linkContainer.style.transition = ''
+        linkContainer.style.transform = getTranslate3dText(0)
+      }
     }
-
-    linkContainer.style.transition = ''
-    linkContainer.style.transform = 'translate3d(0px,0px,0px)'
 
     ongoing = false
   })
