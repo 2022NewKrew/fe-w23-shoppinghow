5dbef068553c8a06a9feeb3a25fcf3e8ad9c51ae
fleek-moon <fleek.moon@kakaocorp.com>
2022-01-12 19:01:52 +0900

    refactor : api를 스토어부분으로 분리

--- client/src/pages/main/index.js
+++ client/src/pages/main/index.js
@@ -2,7 +2,8 @@ import HotDeal from '@/components/main/hot-deal';
 import HotItem from '@/components/main/hot-item';
 import MainBanner from '@/components/main/main-banner';
 import './index.scss';
-import { api } from '@/api';
+import evt from '@/utils/custom-event';
+import store from '@/store';
 
 export default class MainPage {
   state = {
@@ -16,71 +17,30 @@ export default class MainPage {
     const mainContainer = document.createElement('div');
     mainContainer.className = 'main-container';
 
-    this.MainBanner = new MainBanner({ $parent: mainContainer });
+    this.mainBanner = new MainBanner({ $parent: mainContainer });
     this.hotDeal = new HotDeal({ $parent: mainContainer });
     this.hotItem = new HotItem({ $parent: mainContainer });
     $parent.appendChild(mainContainer);
 
-    this.initializeData();
+    evt.subscribe('promotionList', this.handleSubscription.bind(this));
+    evt.subscribe('hotDealList', this.handleSubscription.bind(this));
+    evt.subscribe('hotItemList', this.handleSubscription.bind(this));
+    evt.subscribe('bannerItemList', this.handleSubscription.bind(this));
+
+    store.load('promotionList');
+    store.load('hotDealList');
+    store.load('hotItemList');
+    store.load('bannerItemList');
   }
 
-  async initializeData() {
-    try {
-      await Promise.all([
-        this.initializeBannerItem(),
-        this.initializePromotionList(),
-        this.initializeHotDeal(),
-        this.initializeHotItem(),
-      ]);
-    } catch (e) {
-      alert(e);
-    }
+  handleSubscription(e) {
+    this.setState(e.detail);
   }
 
   setState(newState) {
     this.state = { ...this.state, ...newState };
-    this.MainBanner.setState(this.state);
+    this.mainBanner.setState(this.state);
     this.hotDeal.setState(this.state);
     this.hotItem.setState(this.state);
-  }
-
-  async initializePromotionList() {
-    try {
-      const res = await api.get('/special-exhibition/main');
-      if (!res.success) throw new Error(res.message);
-      this.setState({ promotionList: res.result });
-    } catch (e) {
-      throw e;
-    }
-  }
-
-  async initializeHotDeal() {
-    try {
-      const res = await api.get('/item/hot-deal');
-      if (!res.success) throw new Error(res.message);
-      this.setState({ hotDealList: res.result });
-    } catch (e) {
-      throw e;
-    }
-  }
-
-  async initializeHotItem() {
-    try {
-      const res = await api.get('/item/hot-items');
-      if (!res.success) throw new Error(res.message);
-      this.setState({ hotItemList: res.result });
-    } catch (e) {
-      throw e;
-    }
-  }
-
-  async initializeBannerItem() {
-    try {
-      const res = await api.get('/banner-item');
-      if (!res.success) throw new Error(res.message);
-      this.setState({ bannerItemList: res.result });
-    } catch (e) {
-      throw e;
-    }
   }
 }
