e98732df118306a14b1ca5851ecc1ae4682c60e5
ethan.sglee <ethan.sglee@kakaocorp.com>
2022-01-12 22:59:11 +0900

    이 상품 어때요 탭의 상품 클릭 시 연관 상품 보여주기 기능 구현

--- ethan.sglee/shoppingHow/public/javascripts/recommendation.js
+++ ethan.sglee/shoppingHow/public/javascripts/recommendation.js
@@ -1,15 +1,35 @@
-function addRecommendationHTML(imgSrc, idx) {
-  const target = document.querySelector(".recommendation-items")
+const PICKED_EMPTY_TEMPLATE = `
+<p class="picked-items__empty-text">최근 본 상품이 없습니다</p>
+`
+
+function fillPickedItem(pickedItem, product) {
+  const template = `
+  <div class="picked-items__thumb">
+    <img src="${product.imgSrc}" alt="상품이미지">
+  </div>
+  <div class="picekd-items__detail">
+    <div class="picked-items__title">
+      <strong>${product.title}</strong>
+    </div>
+    <div class="picked-items__link">
+      <p>상품보기></p>
+    </div>
+  </div>
+  `
+  pickedItem.innerHTML = template
+}
+
+function addRecommendationHTML(target) {
   const hotdealItemTpl = 
   `
   <li class="recommendation-items__item">
       <a href="#" class="recommendation-items__link">
           <span class="recommendation-items__thumb">
-              <img src="${imgSrc}" class="recommendation-items__img" alt="">
+              <img src="" alt="">
           </span>
-          <strong class="hot-deal__title">상품</strong>
+          <strong class="recommendation-items__title">상품</strong>
           <div class="recommendation-items__detail-price">
-              <span class="txt-price">${idx + 1},500 </span>
+              <span class="txt-price">0 </span>
           </div>
       </a>
   </li>
@@ -18,10 +38,52 @@ function addRecommendationHTML(imgSrc, idx) {
   target.innerHTML += hotdealItemTpl
 }
 
-const tempImgSrc = '//shop1.daumcdn.net/shophow/c/image/content/set/ad8255/20211014114401830_24251'
-
 export default function initRecommendation() {
+  const recommendationList = document.querySelector(".recommendation-items")
   for (let i=0; i<10; i++) {
-    addRecommendationHTML(tempImgSrc, 1)
+    addRecommendationHTML(recommendationList)
   }
+  const recommendtaionItemArr = recommendationList.querySelectorAll('.recommendation-items__item')
+
+  const productArr = []
+
+  for (let i=0; i<sessionStorage.length; i++) {
+    let key = sessionStorage.key(i)
+    const product = JSON.parse(sessionStorage.getItem(key))
+    productArr.push(product)
+  }
+
+  const pickedList = document.querySelector(".picked-items")
+
+  let max_length = productArr.length < 5 ? productArr.length : 5
+  const pickedItemArr = pickedList.querySelectorAll('.picked-items__item')
+
+  for (let i=0; i<max_length; i++) {
+    fillPickedItem(pickedItemArr[i], productArr[i])
+  }
+
+  pickedList.addEventListener('click', ({target}) => {
+    const selected = pickedList.querySelector('.selected')
+    if (selected) {
+      selected.classList.remove('selected')
+    }
+
+    recommendationList.classList.remove('hidden')
+
+    const pickedItem = target.closest('.picked-items__item')
+    pickedItem.classList.add('selected')  
+
+    const title = pickedItem.querySelector('.picked-items__title > strong').innerText
+    const product = JSON.parse(sessionStorage.getItem(title))
+    
+    recommendtaionItemArr.forEach((item) => {
+      item.querySelector('.recommendation-items__thumb > img').src = product.imgSrc
+      item.querySelector('.recommendation-items__thumb > img').alt = `${title} 사진`
+      item.querySelector('.recommendation-items__title').innerText = title
+      item.querySelector('.txt-price').innerText = product.price
+
+    })
+    
+  })
+
 }
