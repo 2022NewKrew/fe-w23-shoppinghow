644c8350782f8c615bb2ad0f781b21b526fad7fb
ethan-3160 <ethan.3160@kakaocorp.com>
2022-01-12 18:03:05 +0900

    Add: footer section

--- packages/web/src/app.js
+++ packages/web/src/app.js
@@ -1,10 +1,12 @@
 import Header from "./components/header";
 import Main from "./components/main";
 import "src/sass/global.scss";
+import Footer from "./components/footer";
 
 function App($app) {
   new Header({ $app });
   new Main({ $app });
+  new Footer({ $app });
 }
 
 export default App;
