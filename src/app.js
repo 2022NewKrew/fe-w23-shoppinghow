c0bb5a33ce977ca064e22cf4eafb34e919e363b6
vvvpiano <vvvpiano60@gmail.com>
2022-01-13 14:55:58 +0900

    feat: localStorage에 최근본 상품 넣는 기능 구현

--- src/app.js
+++ src/app.js
@@ -3,6 +3,19 @@ import '@sass/app.scss';
 import PageRouter from '@utils/pageRouter';
 import { onLocationChangeHandler } from '@utils/customEventHandler';
 import { headerView } from '@components/headerView';
+import { addItemToLocalStorage } from '@utils/localStorage/localStorageFunctions';
+import { REMOVE_PRE_DUPLICATED_ITEM_CONSTRAINTS } from '@utils/localStorage/constraintsFunction';
+
+const productClickHandler = (e) => {
+    e.preventDefault();
+    let product = e.target.closest('.goods-stall__link');
+    if (!product) return;
+    const newProductData = { id: product.dataset.productId, imgSrc: product.dataset.productImgSrc };
+
+    const IS_SAME_PRODUCT = (pro1, pro2) => pro1.id === pro2.id;
+    const addItemRule = REMOVE_PRE_DUPLICATED_ITEM_CONSTRAINTS(IS_SAME_PRODUCT);
+    addItemToLocalStorage('recentProduct', newProductData, addItemRule);
+};
 
 window.addEventListener('DOMContentLoaded', () => {
     const app = document.querySelector('body');
@@ -17,11 +30,7 @@ window.addEventListener('DOMContentLoaded', () => {
     const pageRouter = new PageRouter(mainContents);
     window.addEventListener('locationchange', (e) => onLocationChangeHandler(e, pageRouter));
     window.addEventListener('popstate', pageRouter.renderPage);
-    app.addEventListener('click', (e) => {
-        e.preventDefault();
-        let product = e.target.closest('.goods-stall__link');
-        console.log([product.dataset.productId, product.dataset.productImgSrc]);
-    });
+    app.addEventListener('click', (e) => productClickHandler(e));
 
     pageRouter.renderPage();
 });
