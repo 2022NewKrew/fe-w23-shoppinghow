6f41d1bee4f23bef87e8f89b2d2bfaa3f3ee45d7
terajh <terajoohyun@ajou.ac.kr>
2022-01-13 14:28:18 +0900

    feat: rolling 데이터 모듈화
    utils/api 로 top10 데이터를 옮겨 api 를 통해 데이터를 받아오는 로직 추가

--- src/public/scripts/utils/api.js
+++ src/public/scripts/utils/api.js
@@ -11,4 +11,11 @@ export const api = {
       return false;
     }
   },
+
+  top10() {
+    const response = {
+      result: ["스노우보드", "스테비아토마토", "led마스크", "런닝머신", "단백질보충제", "앰플", "육수팩", "유산균", "제기세트", "스케이트"],
+    };
+    return response;
+  },
 };
